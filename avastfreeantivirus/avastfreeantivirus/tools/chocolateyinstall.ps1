ecef738f4ef26fb44952eb6ff46982d1b2f8765c
Paul Broadwith <pauby@users.noreply.github.com>
2017-02-03 18:31:50 +0000

    Added new packages and reformatted existing ones

---
+++ avastfreeantivirus/avastfreeantivirus/tools/chocolateyinstall.ps1
@@ -0,0 +1,46 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'avastfreeantivirus'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://files.avast.com/iavs9x/avast_free_antivirus_setup.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'avastfreeantivirus*'
+
+  checksum      = '4A30F70BB12C0701B47C8352B997BD51309EFED8E6EBD61970FCD1ADF2452337'
+  checksumType  = 'sha256'
+  checksum64    = '4A30F70BB12C0701B47C8352B997BD51309EFED8E6EBD61970FCD1ADF2452337'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/silent"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
