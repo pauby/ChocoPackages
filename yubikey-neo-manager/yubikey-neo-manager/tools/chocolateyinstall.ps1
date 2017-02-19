7fc89c835a93634e43d775f7378a47f65bc28858
Paul Broadwith <pauby@users.noreply.github.com>
2017-02-19 21:53:24 +0000

    Final package

--- yubikey-neo-manager/yubikey-neo-manager/tools/chocolateyinstall.ps1
+++ yubikey-neo-manager/yubikey-neo-manager/tools/chocolateyinstall.ps1
@@ -4,30 +4,17 @@ $packageName  = 'yubikey-neo-manager'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = ''
 
-#Based on NO DETECTION IN PRO
 $packageArgs = @{
   packageName   = $packageName
   softwareName  = 'yubikey-neo-manager*'
   fileType      = 'exe'
-  silentArgs    = "/S" # NSIS
-  #OTHERS
-  # Uncomment matching EXE type (sorted by most to least common)
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  #silentArgs   = '/s /v"/qn"' # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s'           # Ghost
-  # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
-  #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://chocolatey.org/packages/autohotkey.portable
+  silentArgs    = "/S"
   
-  validExitCodes= @(0) #please insert other valid exit codes here
-  url           = "https://developers.yubico.com/yubikey-neo-manager/Releases/yubikey-neo-manager-1.4.0-win.exe"  #download URL, HTTPS preferrred
+  validExitCodes= @(0)
+  url           = "https://developers.yubico.com/yubikey-neo-manager/Releases/yubikey-neo-manager-1.4.0-win.exe"
   checksum      = 'D7D03379AF80AE15487106C685DCEACF1851D6D4D59BDE29117C42B9B83167E2'
   checksumType  = 'sha256'
-  url64bit      = ""   # 64bit URL here (HTTPS preferred) or remove - if installer contains both architectures (very rare), use $url
+  url64bit      = ""
   checksum64    = ''
   checksumType64= 'sha256'
   destination   = $toolsDir
@@ -35,5 +22,4 @@ $packageArgs = @{
 
 Install-ChocolateyPackage @packageArgs 
 
-## See https://chocolatey.org/docs/helpers-reference
 
