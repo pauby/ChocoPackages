2c6382ef7a9ae25061f8c0e4cbf0aac9b729c26f
Paul Broadwith <pauby@users.noreply.github.com>
2017-03-19 15:52:08 +0000

    Initial commit

---
+++ outlookcaldav/outlookcaldav/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+$packageName  = 'outlookcaldav'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://github.com/aluxnimm/outlookcaldavsynchronizer/releases/download/v2.17.0/OutlookCalDavSynchronizer-2.17.0.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  file			    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\\CalDavSynchronizer.Setup.msi"
+
+  softwareName  = 'OutlookCalDavSynchronizer*'
+
+  checksum      = '928FA2ED6CE3A6297502C21A294A443B883A82605758837B8691C9EFECBDF809'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/quiet"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
