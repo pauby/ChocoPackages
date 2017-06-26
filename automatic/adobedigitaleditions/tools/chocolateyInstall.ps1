ce3b49e0bf284c731c73b8c665b7c43f19a53394
Paul Broadwith <paul@pauby.com>
2017-06-26 20:37:44 +0100

    Removed 64bit variables

--- automatic/adobedigitaleditions/tools/chocolateyInstall.ps1
+++ automatic/adobedigitaleditions/tools/chocolateyInstall.ps1
@@ -3,21 +3,17 @@
 $packageName= 'adobedigitaleditions'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://adedownload.adobe.com/pub/adobe/digitaleditions/ADE_4.5_Installer.exe'
-$url64      = ''
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'adobedigitaleditions*'
 
   checksum      = '9750099afe7321eeb7d7c0f916c1d0b3cc5921e233fafcd3b7719ea02fbb86e1'
   checksumType  = 'SHA256'
-  checksum64    = ''
-  checksumType64= 'sha256'
 
   silentArgs   = '/S'
   # Note code 1223 is added as valid for v4.5.5 as it exits with this code but appears to install correctly
