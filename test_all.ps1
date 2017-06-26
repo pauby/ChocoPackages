534339df090d21da0106023cb5a3914a82a3c82f
Paul Broadwith <paul@pauby.com>
2017-06-26 09:47:04 +0100

    Updated with chocolatey au templates

--- test_all.ps1
+++ test_all.ps1
@@ -13,31 +13,31 @@ if (($Name.Length -gt 0) -and ($Name[0] -match '^random (.+)')) {
     Write-Host "TESTING GROUP $($n+1) of $group"
 
     $group_size = [int]($lsau.Count / $group) + 1
-    $Name = $lsau | select -First $group_size -Skip ($group_size*$n) | % { $_.Name }
+    $Name = $lsau | select -First $group_size -Skip ($group_size * $n) | % { $_.Name }
 
     Write-Host ($Name -join ' ')
-    Write-Host ('-'*80)
+    Write-Host ('-' * 80)
 }
 
 $options = [ordered]@{
-    Force = $true
-    Push = $false
+    Force  = $true
+    Push   = $false
 
     Report = @{
-        Type = 'markdown'                                   #Report type: markdown or text
-        Path = "$PSScriptRoot\Update-Force-Test-${n}.md"      #Path where to save the report
-        Params= @{                                          #Report parameters:
+        Type   = 'markdown'                                   #Report type: markdown or text
+        Path   = "$PSScriptRoot\Update-Force-Test-${n}.md"      #Path where to save the report
+        Params = @{                                          #Report parameters:
             Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
-            NoAppVeyor  = $false                            #  Markdown: do not show AppVeyor build shield
-            Title       = "Update Force Test - Group ${n}"
-            UserMessage = "[Update report](https://gist.github.com/$Env:gist_id) | **USING AU NEXT VERSION**"       #  Markdown, Text: Custom user message to show
+            NoAppVeyor      = $false                            #  Markdown: do not show AppVeyor build shield
+            Title           = "Update Force Test - Group ${n}"
+            UserMessage     = "[Update report](https://gist.github.com/$Env:gist_id) | **USING AU NEXT VERSION**"       #  Markdown, Text: Custom user message to show
         }
     }
 
-    Gist = @{
-        Id     = $Env:gist_id_test                          #Your gist id; leave empty for new private or anonymous gist
-        ApiKey = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
-        Path   = "$PSScriptRoot\Update-Force-Test-${n}.md"       #List of files to add to the gist
+    Gist   = @{
+        Id          = $Env:gist_id_test                          #Your gist id; leave empty for new private or anonymous gist
+        ApiKey      = $Env:github_api_key                        #Your github api key - if empty anoymous gist is created
+        Path        = "$PSScriptRoot\Update-Force-Test-${n}.md"       #List of files to add to the gist
         Description = "Update Force Test Report #powershell #chocolatey"
     }
 }
