cbd74b559f4c7ec6d8484918a9bb7e99bd1a950f
Paul Broadwith <paul@pauby.com>
2017-06-26 18:35:43 +0100

    Added package to AU

---
+++ automatic/gitter/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'gitter'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://update.gitter.im/win/GitterSetup-3.1.0.exe'
+
+$packageArgs = @{
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'EXE'
+    url            = $url
+
+    softwareName   = 'Gitter'
+    checksum       = '897c7a0e2b45f57c2e2651b7cca795f681407378d5974d3694dfd986070fca7b'
+    checksumType   = 'sha256'
+
+    silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /DIR=$env:chocolateyPackageFolder"
+
+    validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+# Start Menu shortcuts are created under the admin account - move them to All Users start menu
+Move-Item -Path (Join-Path -Path $env:USERPROFILE -ChildPath "AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Gitter") `
+	-Destination (Join-Path -Path $env:ALLUSERSPROFILE -ChildPath "Microsoft\Windows\Start Menu\Programs")
---
+++ automatic/gitter/update.ps1
@@ -0,0 +1,34 @@
+import-module au
+
+. $PSScriptRoot\..\..\scripts\all.ps1
+
+$releases    = 'https://update.gitter.im/win/latest'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*\$url\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
+function global:au_AfterUpdate { 
+    Set-DescriptionFromReadme -SkipFirst 2 
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $releases
+    $regexUrl = "GitterSetup-(.*).exe"
+
+    $url = $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href  
+    $version = $matches[1]
+
+    return @{
+        URL32        = $url
+        Version      = $version
+    }
+}
+
+update -ChecksumFor 32
