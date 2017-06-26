f01662358e8e83fd5c3049101b56aba01d706dd0
Paul Broadwith <paul@pauby.com>
2017-06-26 16:24:58 +0100

    Moved package to AU

---
+++ automatic/avastfreeantivirus/README.md
@@ -0,0 +1,6 @@
+# ![Avast Free Antivirus](https://cdn.rawgit.com/pauby/ChocoPackages/6cf2f9f5/icons/avastfreeantivirus.png "Avast Free Antivirus") [Avast Free Antivirus](https://chocolatey.org/packages/avastfreeantivirus)
+
+Free antivirus software from Avast.
+
+    **NOTE:** This package can take upwards of 10 minutes to complete the install (depending on your computer specs and internet bandwidth).
+    **NOTE:** The uninstall for this package **is not silent**.
--- automatic/avastfreeantivirus/tools/chocolateyinstall.ps1
+++ automatic/avastfreeantivirus/tools/chocolateyinstall.ps1
@@ -2,21 +2,17 @@
 
 $packageName= 'avastfreeantivirus'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://files.avast.com/iavs9x/avast_free_antivirus_setup.exe'
-$url64      = ''
+$url        = 'http://files.avast.com/iavs9x/avast_free_antivirus_setup_online.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
 
   softwareName  = 'Avast Free Antivirus'
-  checksum      = 'F8151FCEF0D692D929E0729772B93B7F1887A3D9399733670CB266EC98C2B2DE'
-  checksumType  = 'sha256'
-  checksum64    = ''
-  checksumType64= 'sha256'
+  checksum      = ''
+  checksumType  = ''
 
   silentArgs    = "/silent"
   validExitCodes= @(0)
