ac6f16df033576dedb266275b2fa774da6fbd273
Paul Broadwith <paul@pauby.com>
2017-07-03 22:43:28 +0100

    New package

---
+++ automatic/yubikey-piv-manager/README.md
@@ -0,0 +1,13 @@
+# ![Yubikey PIV Manager](https://cdn.rawgit.com/pauby/ChocoPackages/8002542d/icons/yubikey-piv-manager.png "Yubikey PIV Manager Logo") [Yubikey PIV Manager](https://chocolatey.org/packages/yubikey-piv-manager)
+
+Graphical application for configuring a PIV-enabled YubiKey.
+
+![Screenshot](https://github.com/pauby/chocopackages/tree/master/automatic/yubikey-piv-manager/screenshot.png)
+
+## What Is PIV?
+
+PIV, or FIPS 201, is a US government standard. It enables RSA or ECC sign/encrypt operations using a private key stored on a smartcard (such as the YubiKey NEO), through common interfaces like PKCS#11.
+
+You can read more about the PIV standards here: http://csrc.nist.gov/groups/SNS/piv/standards.html
+
+PIV is primarily used for non-web applications. It has built-in support under Windows, and can be used on OS X and Linux via the OpenSC project.
---
+++ automatic/yubikey-piv-manager/tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'yubikey-piv-manager'
+$url = 'https://github.com//Yubico/yubikey-piv-manager/releases/download/yubikey-piv-manager-1.4.2/yubikey-piv-manager-1.4.2-win.exe'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName    = $packageName
+    softwareName   = 'yubikey piv manager*'
+    fileType       = 'exe'
+    silentArgs     = "/S"
+  
+    validExitCodes = @(0)
+    url            = $url
+    checksum       = 'fd6f3ef1497c065731f4c7dd270f99aebaba78ac63be71d62e8a9ec073b7de90'
+    checksumType   = 'sha256'
+
+    destination    = $toolsDir
+}
+
+Install-ChocolateyPackage @packageArgs 
+
+# add start menu shortcuts
+if (Get-OSArchitectureWidth -eq "64") {
+    $targetPath = "C:\Program Files (x86)\Yubico\YubiKey PIV Manager"
+}
+else {
+    $targetPath = "C:\Program Files\Yubico\YubiKey PIV Manager"
+}
+
+$menuPath = (Join-Path -Path $env:APPDATA -ChildPath "Microsoft\Windows\Start Menu\Programs\Yubico\Yubikey PIV Manager")
+
+Install-ChocolateyShortcut `
+    -ShortcutFilePath (Join-Path -Path $menuPath -ChildPath "Yubikey PIV Manager.lnk") `
+    -TargetPath (Join-Path -Path $targetPath -ChildPath "pivman.exe")
+
+Install-ChocolateyShortcut `
+    -ShortcutFilePath (Join-Path -Path $menuPath -ChildPath "Uninstall PIV Manager.lnk") `
+    -TargetPath (Join-Path -Path $targetPath -ChildPath "uninstall.exe")
