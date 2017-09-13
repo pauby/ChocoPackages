130aa5d70c40bc3bf811c14d2be56f6b11445c88
Chocolatey <chocolatey@realdimensions.net>
2017-09-13 00:10:11 +0000

    AU: 1 updated - outlookcaldav [skip ci] https://gist.github.com/e3b29a09c77246998b82c9ad33f4be4a/d57ce29d92b4639533fc8062504230af5c77455a

--- automatic/outlookcaldav/tools/chocolateyinstall.ps1
+++ automatic/outlookcaldav/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageName  = 'outlookcaldav'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://github.com//aluxnimm/outlookcaldavsynchronizer/releases/download/v2.23.0/OutlookCalDavSynchronizer-2.23.0.zip'
+$url          = 'https://github.com//aluxnimm/outlookcaldavsynchronizer/releases/download/v2.24.0/OutlookCalDavSynchronizer-2.24.0.zip'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -16,7 +16,7 @@ $packageArgs = @{
 
   softwareName  = 'OutlookCalDavSynchronizer*'
 
-  checksum      = 'b065e432932a3480821d2dbd25387750634a51d0b123e866a05a90483601158b'
+  checksum      = '48e56dd095396de16393d818d7574734c8c743261f3d5d5abac2641ed288946e'
   checksumType  = 'SHA256'
 
   silentArgs    = "/quiet"
