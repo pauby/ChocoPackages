6614a56bb84a37706c3c67b1154e3b87f5ecc2a5
Paul Broadwith <paul@pauby.com>
2017-09-21 12:36:44 +0100

    Initial commit

---
+++ automatic/focusatwill/README.md
@@ -0,0 +1,10 @@
+# ![Focus@Will](https://cdn.rawgit.com/pauby/ChocoPackages/bca139e8/icons/focusatwill.png "Focus@Will Logo") [Focus@Will](https://chocolatey.org/packages/focusatwill)
+
+Scientists have discovered that depending on your personality type, there is a specific type of “music” that when engineered just right, puts your brain into a “flow state” making you hyper-focused and exponentially more productive.
+
+We'll ask you a few questions, determine the perfect music for you, and give you the scientifically engineered music channel that has been proven to 4x your focus and optimize your productivity. You'll still have access to over 50 other channels but you'll know exactly what channels create the best focus for your unique personality.
+
+* Over 50 diverse channels
+* Thousands of hours of unique tracks
+* Exclusively commissioned and remixed
+* Based on proven neuroscience research
---
+++ automatic/focusatwill/update.ps1
@@ -0,0 +1,56 @@
+import-module au
+
+. $PSScriptRoot\..\..\scripts\all.ps1
+
+$releases    = 'https://support.techsmith.com/hc/en-us/articles/115006443267%C2%A0'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*\$url64\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate() {
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+    $Latest.ChecksumType64 = 'SHA256'
+}
+
+function global:au_AfterUpdate { 
+    Set-DescriptionFromReadme -SkipFirst 2 
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $releases
+    $regexUrl = ": Camtasia \(Windows\) ([\d\.]+)"
+
+    $page.content -match $regexUrl
+    $version = $matches[1]
+
+    $urlVer = $version.Replace('.', '')
+    $url = "https://download.techsmith.com/camtasiastudio/enu/$urlVer/camtasia.msi"
+
+    # As we messed up the versions a little we need to make sure the minor vwersion number is 2 digits long and has trailing zeroes.
+    # When Camtasia reaches major version 10 we can correct this.
+    # Chocolatey (or Nuget specs to be more precise) ignore any leading zeroes. and for some reason version 9.0.5 (and earlier) of Camtastia was being 
+    # reported as 9.05 - that needs looked into too but in the meantime this is a fix.
+
+    # extract the minor version
+    $version -match "\d\.(\d)" | Out-Null
+    $minor = if ($matches[1].length -lt 2 -and $matches[1][0] -ne "0") { 
+        "$($matches[1])0" 
+    } else { 
+        $matches[1]
+    }
+    $version = $version -replace "(\d)\.\d\.(\d)", "`$1.$minor.`$2"
+    
+    return @{
+        URL64        = $url
+        Version      = $version
+    }
+}
+
+update -ChecksumFor none
