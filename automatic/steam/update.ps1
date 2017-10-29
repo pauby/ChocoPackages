5fc5bc193e726cc607b7a8043a8800acab6fcb25
Paul Broadwith <paul@pauby.com>
2017-10-29 15:00:04 +0000

    Fixed regex typo [AU steam:2.10.91.9100]

--- automatic/steam/update.ps1
+++ automatic/steam/update.ps1
@@ -7,7 +7,7 @@ $releases = 'https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            '(^\s*\$url\s*=\s*)(''.*'')'          = "`$1'$($Latest.URL32)'"
+            '(^\s*\url\s*=\s*)(''.*'')'           = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
             "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
         }
@@ -24,8 +24,10 @@ function global:au_GetLatest {
     $version = (Get-Item $tempfile).VersionInfo.FileVersion
 
     return @{
-        URL32   = $releases
-        Version = $version
+        URL32           = $releases
+        Version         = $version
+        Checksum32      = ((Get-FileHash -Path $tempFile -Algorithm SHA256).Hash)
+        ChecksumType32  = 'SHA256' 
     }
 }
 
