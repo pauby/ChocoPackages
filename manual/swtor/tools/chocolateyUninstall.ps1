cccd468911a54315c3dec847d11d8a0fe0a32eb2
Paul Broadwith <paul@pauby.com>
2017-10-29 15:39:53 +0000

    Removed unused var and replaced aliases

--- manual/swtor/tools/chocolateyUninstall.ps1
+++ manual/swtor/tools/chocolateyUninstall.ps1
@@ -15,10 +15,9 @@ $ahkFile = Join-Path -Path $env:TEMP -ChildPath "$(Get-Random).ahk"
 $ahkSourceFile = Join-Path -Path $toolsDir -ChildPath "$($env:ChocolateyPackageName)_uninstall.ahk"
 Copy-Item -Path $ahkSourceFile -Destination $ahkFile
 
-$uninstalled = $false
 [array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs.softwareName
 if ($key.Count -eq 1) {
-    $key | % {
+    $key | ForEach-Object {
         $packageArgs.file = $_.UninstallString
 
         Write-Verbose "Running AutoHotkey install script $ahkFile"
@@ -35,7 +34,7 @@ if ($key.Count -eq 1) {
     Write-Warning "$key.Count matches found!"
     Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
     Write-Warning "Please alert package maintainer the following keys were matched:"
-    $key | % {Write-Warning "- $_.DisplayName"}
+    $key | ForEach-Object {Write-Warning "- $_.DisplayName"}
 }
 
 Remove-Item $ahkFile -Force
