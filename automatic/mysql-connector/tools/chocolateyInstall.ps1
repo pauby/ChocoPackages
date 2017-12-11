74ed3a902e48060a3c5e448fbae7b78fa0219e75
Chocolatey <chocolatey@realdimensions.net>
2017-12-11 11:24:59 +0000

    AU: 1 updated - mysql-connector [skip ci] https://gist.github.com/e3b29a09c77246998b82c9ad33f4be4a/792d6a6bc911ee0f91ad58d0d1613f807a83abdf

--- automatic/mysql-connector/tools/chocolateyInstall.ps1
+++ automatic/mysql-connector/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'mysql-connector'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://dev.mysql.com/get/Downloads/Connector-Net/mysql-connector-net-6.10.4.msi'
+$url = 'https://dev.mysql.com/get/Downloads/Connector-Net/mysql-connector-net-6.10.5.msi'
 
 
 $packageArgs = @{
@@ -13,7 +13,7 @@ $packageArgs = @{
 
     softwareName   = 'mysql connector net*'
 
-    checksum       = '90e6d312b46c3519851e9100c40da356e7ecb06d189bcc80a2bed0c6fa7c8a16'
+    checksum       = '28e025d89f743f7ad707d0e7f2cc04441e1c4572eb5e00c9c02ea8ed48f6253a'
     checksumType   = 'SHA256'
 
     silentArgs     = '/qn'
