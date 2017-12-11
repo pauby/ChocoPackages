714878292a302150ba500278188782b1abc48ee5
Chocolatey <chocolatey@realdimensions.net>
2017-12-11 12:09:38 +0000

    AU: 1 updated - iris [skip ci] https://gist.github.com/e3b29a09c77246998b82c9ad33f4be4a/75b9ac010baf560a5a17e36d872e1f9fd68d0772

--- automatic/iris/tools/chocolateyinstall.ps1
+++ automatic/iris/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = 'https://raw.githubusercontent.com/danielng01/Iris-Builds/master/Windows/Iris-0.9.2.8.exe'
-    checksum       = 'd93b4402af446bcae413899e771ba23abb1c6946f2d07c7ae31b566b3b9ef1b6'
+    url            = 'https://raw.githubusercontent.com/danielng01/Iris-Builds/master/Windows/Iris-0.9.3.exe'
+    checksum       = '5de0f205b5f5b49a856ffb9b7626b2e346ffb7998e234bbbecee9ed61826022d'
     checksumType   = 'SHA256'
     fileType       = 'EXE'
     silentArgs     = '/S'
