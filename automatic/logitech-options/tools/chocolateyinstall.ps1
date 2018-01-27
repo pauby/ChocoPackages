f037f67e3320039d8ab8cc5e87792982df08d899
Paul Broadwith <paul@pauby.com>
2018-01-27 17:47:26 +0000

    Initial commit [AU logitech-options:6.72.344]

---
+++ automatic/logitech-options/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    url            = 'https://download01.logi.com/web/ftp/pub/techsupport/options/Options_6.72.344.exe'
+    checkum        = 'D1A44FA96049B8B0F9FB8A0B719588D7AE0DFEBE7770CA26AFF0A35D27FE502A'
+    checksumType   = 'sha256'
+    fileType       = 'EXE'
+    silentArgs     = ''
+    validExitCodes = @(0)   
+}
+
+$ahkExe = 'AutoHotKey'
+$ahkFile = Join-Path -Path $env:TEMP -ChildPath "$(Get-Random).ahk" 
+$ahkSourceFile = Join-Path -Path $toolsDir -ChildPath "$($env:ChocolateyPackageName)_install.ahk"
+Copy-Item -Path $ahkSourceFile -Destination $ahkFile
+
+Write-Verbose "Running AutoHotkey install script $ahkFile"
+$ahkProc = Start-Process -FilePath $ahkExe -ArgumentList $ahkFile -PassThru
+$ahkId = $ahkProc.Id
+Write-Debug "$ahkExe start time:`t$($ahkProc.StartTime.ToShortTimeString())"
+Write-Debug "Process ID:`t$ahkId"
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/logitech-options/update.ps1
@@ -0,0 +1,41 @@
+import-module au
+
+. $PSScriptRoot\..\..\scripts\all.ps1
+
+$releases    = 'http://support.logitech.com/en_gb/software/options'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*\$url\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+        }
+    }
+}
+
+function global:au_BeforeUpdate() {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+    $Latest.ChecksumType32 = 'SHA256'
+}
+
+function global:au_AfterUpdate { 
+    Set-DescriptionFromReadme -SkipFirst 2 
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $regex = '(?ms)Software Version:\s*<span>(?<version>\d+\.\d+\.\d+)</span>'
+    if ($page.content -match $regex) {
+        $version = $matches.version
+    }
+    $url = "https://download01.logi.com/web/ftp/pub/techsupport/options/Options_$version.exe"
+
+    return @{
+        URL32        = $url
+        Version      = $version
+    }
+}
+
+Update-Package -ChecksumFor none
