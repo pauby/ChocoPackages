2b53e98282ef6600c1c518f02585f3e450e48c60
Paul Broadwith <paul@pauby.com>
2018-01-29 19:13:08 +0000

    Updated checksum [AU keepass.install:2.38]

--- automatic/keepass.install/tools/chocolateyInstall.ps1
+++ automatic/keepass.install/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.38/KeePass-2.38-Setup.exe/download'
-    checksum       = '7099964cbb1a5829fa4be416bf7b6a657c689e4ecd2c36112e7101f4ddb09895'
+    checksum       = '400B66380D30C904711BA3B017AA97F9E67081B73D6A239C6C44AC2B663CB23B'
     checksumType   = 'SHA256'
     fileType       = 'EXE'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
