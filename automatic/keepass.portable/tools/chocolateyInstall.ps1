09e8c41ce0747190e8bde859fbc295fd71b16677
Paul Broadwith <paul@pauby.com>
2018-02-04 15:32:07 +0000

    Initial commit [AU keepass.portable:2.38]

---
+++ automatic/keepass.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.38/KeePass-2.38.zip/download'
+    checksum       = 'db71cba72c321ecaeda7eec6f4e71e215864d4113ab9e29dc97ad4d56c80a7a9'
+    checksumType   = 'sha256'
+    UnzipLocation  = $toolsDir
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Set-Content -Path (Join-Path -Path $toolsDir -ChildPath "KeePass.exe.gui") -Value $null
