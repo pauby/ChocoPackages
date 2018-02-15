105351b3d8261c1e2ff31cde7589fd3e6a8fde14
Chocolatey <chocolatey@realdimensions.net>
2018-02-15 18:11:46 +0000

    AU: 1 updated - yubico-authenticator [skip ci] https://gist.github.com/e3b29a09c77246998b82c9ad33f4be4a/686ec455f979f1abcf9eabd7766876ce6349520a

--- automatic/yubico-authenticator/tools/chocolateyinstall.ps1
+++ automatic/yubico-authenticator/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName  = 'yubico-authenticator'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://github.com//Yubico/yubioath-desktop/releases/download/yubioath-desktop-4.3.2/yubioath-desktop-4.3.2-win.exe'
+$url          = 'https://github.com//Yubico/yubioath-desktop/releases/download/yubioath-desktop-4.3.3/yubioath-desktop-4.3.3-win.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -11,7 +11,7 @@ $packageArgs = @{
   silentArgs    = "/S" # NSIS 
   validExitCodes= @(0)
   url			      = $url
-  checksum      = 'c894808a341fafc176476c045bc1e26b5b878e194624c33e1a6b24382a353dc5'
+  checksum      = '5a13fde898ab9f225cb164f737f8525e112250e95c0abed2cc87003bd3d468af'
   checksumType  = 'sha256'
   destination   = $toolsDir
 }
