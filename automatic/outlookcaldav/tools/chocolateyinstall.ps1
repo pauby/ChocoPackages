cd46b64aa308da089a14abeb9ba3b227a3262776
Paul Broadwith <paul@pauby.com>
2018-04-13 20:21:44 +0100

    Refactored code [skip ci]

--- automatic/outlookcaldav/tools/chocolateyinstall.ps1
+++ automatic/outlookcaldav/tools/chocolateyinstall.ps1
@@ -1,43 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-function ConvertFrom-ChocoParameters ([string]$parameter) {
-    $arguments = @{};
-
-    if ($parameter) {
-        $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"
-        $option_name = 'option'
-        $value_name = 'value'
-
-        if ($parameter -match $match_pattern) {
-            $results = $parameter | Select-String $match_pattern -AllMatches
-            $results.matches | ForEach-Object {
-                $arguments.Add(
-                    $_.Groups[$option_name].Value.Trim(),
-                    $_.Groups[$value_name].Value.Trim())
-            }
-        }
-        else {
-            throw "Package Parameters were found but were invalid (REGEX Failure). See package description for correct format."
-        }
-    }
-
-    return $arguments
-}
-
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$packageName  = 'outlookcaldav'
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://github.com//aluxnimm/outlookcaldavsynchronizer/releases/download/v3.1.0/OutlookCalDavSynchronizer-3.1.0.zip'
 
 $packageArgs = @{
-  packageName   = $packageName
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
-  url           = $url
-  file          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\\CalDavSynchronizer.Setup.msi"
-
-  softwareName  = 'OutlookCalDavSynchronizer*'
+  url           = 'https://github.com//aluxnimm/outlookcaldavsynchronizer/releases/download/v3.1.0/OutlookCalDavSynchronizer-3.1.0.zip'
+  file          = Join-Path -Path $toolsDir -ChildPath 'CalDavSynchronizer.Setup.msi'
 
   checksum      = 'f4144f93a5157ef894b8f7a2ceb4a8cbfa5e6ab23c4ac93b5b8678c5487e09e1'
   checksumType  = 'SHA256'
@@ -46,10 +16,14 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1641)
 }
 
-$arguments = ConvertFrom-ChocoParameters -Parameter $env:chocolateyPackageParameters
+$arguments = Get-PackageParameters -Parameter $env:ChocolateyPackageParameters
 if ($arguments.ContainsKey("allusers")) {
     $packageArgs.silentArgs += " ALLUSERS=1"
 }
 
+# Unzip the file to the toolsdir and then install the MSI
 Install-ChocolateyZipPackage @packageArgs
 Install-ChocolateyInstallPackage @packageArgs
+
+# Create a shim ignore
+New-Item -Name 'setup.exe.ignore' -Path $toolsDir -ItemType File -ErrorAction SilentlyContinue | Out-Null
--- automatic/outlookcaldav/tools/chocolateyuninstall.ps1
+++ automatic/outlookcaldav/tools/chocolateyuninstall.ps1
@@ -1,40 +1,33 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'OutlookCalDav'
-$softwareName = 'OutlookCalDav*' 
-$installerType = 'ZIP' 
-
-$silentArgs = '/qn /norestart'
-$validExitCodes = @(0, 3010, 1605, 1614, 1641)
-if ($installerType -ne 'MSI') {
-
-  $validExitCodes = @(0)
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    softwareName   = 'CalDavSynchronizer*' 
+    fileType       = 'MSI'
+    silentArgs     = '/qn /norestart'
+    validExitCodes = @(0, 3010, 1605, 1614, 1641)
 }
 
-$uninstalled = $false
-[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
-
+[array]$key = Get-UninstallRegistryKey @packageArgs
 if ($key.Count -eq 1) {
-  $key | % { 
-    $file = "$($_.UninstallString)"
+    $key | ForEach-Object { 
+        $packageArgs['file'] = "$($_.UninstallString)"
+        if ($packageArgs['fileType'] -eq 'MSI') {
+            $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+            $packageArgs['file'] = ''
+        }
 
-    if ($installerType -eq 'MSI') {
-      $silentArgs = "$($_.PSChildName) $silentArgs"
-
-      $file = ''
+        Uninstall-ChocolateyPackage @packageArgs
     }
-
-    Uninstall-ChocolateyPackage -PackageName $packageName `
-                                -FileType $installerType `
-                                -SilentArgs "$silentArgs" `
-                                -ValidExitCodes $validExitCodes `
-                                -File "$file"
-  }
-} elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$key.Count matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $_.DisplayName"}
+}
+elseif ($key.Count -eq 0) {
+    Write-Warning "$env:ChocolateyPackageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$key.Count matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | ForEach-Object {
+        Write-Warning "- $_.DisplayName"
+    }
 }
