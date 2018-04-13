d25e8a4a9b5c965f1cb9b12d9f2dbaa649194090
Paul Broadwith <paul@pauby.com>
2018-04-13 21:09:35 +0100

    Refactored code [skip ci]

--- automatic/streamdeck/tools/chocolateyinstall.ps1
+++ automatic/streamdeck/tools/chocolateyinstall.ps1
@@ -1,28 +1,27 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$packageName  = 'streamdeck'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64        = 'https://edge.elgato.com/egc/windows/sd/Stream_Deck_2.2.0.5165.msi'
-
-. "$toolsDir\PSPackageExtensions.ps1"
-
-$tempName     = Get-ChocoUniqueTempName 
 
 $packageArgs = @{
-  packageName     = $packageName
-  fileType        = 'MSI'
-  fileFullPath    = ""
-  url64           = $url64
-  softwareName    = 'Elgato Stream Deck*'
+    packageName     = $env:ChocolateyPackageName
+    fileType        = 'MSI'
+    url64           = 'https://edge.elgato.com/egc/windows/sd/Stream_Deck_2.2.0.5165.msi'
+    softwareName    = 'Elgato Stream Deck*'
 
-  checksum64      = 'eba8372f26e92ac9d92ee0edc9051f9adceabbce72bf10f5ecfa2d4bcd54584a'
-  checksumType64  = 'SHA256'
+    checksum64      = 'eba8372f26e92ac9d92ee0edc9051f9adceabbce72bf10f5ecfa2d4bcd54584a'
+    checksumType64  = 'SHA256'
 
-  silentArgs      = "/quiet"
-  validExitCodes  = @(0, 3010, 1641)
+    silentArgs      = "/quiet"
+    validExitCodes  = @(0, 3010, 1641)
 }
-$packageArgs.fileFullPath = Join-Path -Path (get-ChocoUniqueTempName) -ChildPath "$packageName.$($packageArgs.fileType.ToLower())"
 
+# get a temporary directory
+do {
+    $tempPath = Join-Path -Path $env:TEMP -ChildPath ([System.Guid]::NewGuid().ToString())
+} while (Test-Path $tempPath)
+$packageArgs.fileFullPath = Join-Path -Path $tempPath -ChildPath "$packageName.$($packageArgs.fileType.ToLower())"
+
+# Check OS version
 write-debug "OS Name: $($env:OS_NAME)"
 if ($env:OS_NAME -ne "Windows 10") {
     throw "Cannot be installed on this version of Windows - $packageName only supports Windows 10 x64 ($($env:OS_NAME))."
