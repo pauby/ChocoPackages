a878891adf61d1debafe50882119a8d7b6257b4f
Paul Broadwith <paul@pauby.com>
2018-04-13 17:52:31 +0100

    Added missing function

--- automatic/f.lux.install/tools/chocolateyinstall.ps1
+++ automatic/f.lux.install/tools/chocolateyinstall.ps1
@@ -1,5 +1,29 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+function ConvertFrom-ChocoParameters ([string]$parameter) {
+    $arguments = @{};
+
+    if ($parameter) {
+        $match_pattern = "\/(?<option>([a-zA-Z]+)):(?<value>([`"'])?([a-zA-Z0-9- _\\:\.]+)([`"'])?)|\/(?<option>([a-zA-Z]+))"
+        $option_name = 'option'
+        $value_name = 'value'
+
+        if ($parameter -match $match_pattern) {
+            $results = $parameter | Select-String $match_pattern -AllMatches
+            $results.matches | ForEach-Object {
+                $arguments.Add(
+                    $_.Groups[$option_name].Value.Trim(),
+                    $_.Groups[$value_name].Value.Trim())
+            }
+        }
+        else {
+            throw "Package Parameters were found but were invalid (REGEX Failure). See package description for correct format."
+        }
+    }
+
+    return $arguments
+}
+
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     url            = 'https://justgetflux.com/flux-setup.exe'
@@ -11,3 +35,16 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
+
+# only create the shortcut in startup if the /noautostart parameter has not been passed
+$arguments = ConvertFrom-ChocoParameters -Parameter $env:ChocolateyPackageParameters
+if (-not $arguments.ContainsKey("noautostart")) {
+    $params = @{
+        ShortcutFilePath    = Join-Path -Path ([Environment]::GetFolderPath('Startup')) -ChildPath 'f.lux.lnk'
+        TargetPath          = Join-Path -Path ([Environment]::GetFolderPath('LocalApplicationData')) -ChildPath 'FluxSoftware\Flux\flux.exe'
+        WorkingDirectory    = Join-Path -Path ([Environment]::GetFolderPath('LocalApplicationData')) -ChildPath 'FluxSoftware\Flux\runtime'
+        Arguments           = '/noshow'
+    }
+    
+    Install-ChocolateyShortcut @params
+}
