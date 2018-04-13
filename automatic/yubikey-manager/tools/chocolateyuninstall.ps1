453f04c362d8dc4c0b5321f28c672bc859cf7303
Paul Broadwith <paul@pauby.com>
2018-04-13 21:34:11 +0100

    Refactored code [skip ci]

--- automatic/yubikey-manager/tools/chocolateyuninstall.ps1
+++ automatic/yubikey-manager/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -9,27 +9,27 @@ $packageArgs = @{
   validExitCodes= @(0)
 }
 
-$uninstalled = $false
 [array]$key = Get-UninstallRegistryKey @packageArgs
-
 if ($key.Count -eq 1) {
-  $key | % { 
-    $packageArgs['file'] = "$($_.UninstallString)"
-    if ($packageArgs['fileType'] -eq 'MSI') {
-      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
-      $packageArgs['file'] = ''
+    $key | ForEach-Object { 
+        $packageArgs.file = "$($_.UninstallString)"
+
+        if ($packageArgs.fileType -eq 'MSI') {
+            $packageArgs.silentArgs = "$($_.PSChildName) $($packageArgs.silentArgs)"
+            $packageArgs.file = ''
+        }
+
+        Uninstall-ChocolateyPackage @packageArgs
     }
-
-    Uninstall-ChocolateyPackage @packageArgs
-  }
-} elseif ($key.Count -eq 0) {
-  Write-Warning "$env:ChocolateyPackageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$key.Count matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $_.DisplayName"}
 }
-
-
-
+elseif ($key.Count -eq 0) {
+    Write-Warning "$env:ChocolateyPackageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$key.Count matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | ForEach-Object {
+        Write-Warning "- $_.DisplayName"
+    }
+}
