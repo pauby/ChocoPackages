549687945421a94a5b636089e55389bbbafcce0d
Paul Broadwith <paul@pauby.com>
2018-04-13 18:21:20 +0100

    Added sleep after killing process

--- automatic/f.lux.portable/tools/chocolateyBeforeModify.ps1
+++ automatic/f.lux.portable/tools/chocolateyBeforeModify.ps1
@@ -2,3 +2,5 @@ $ErrorActionPreference = 'Stop'
 
 Write-Verbose "Stopping the 'flux' process if it is running."
 Get-Process -Name 'flux' -ErrorAction SilentlyContinue | Stop-Process
+
+Start-Sleep -Milliseconds 500
