1d7e4d34082947c6e5d93d8818749ac029c50ea9
Paul Broadwith <paul@pauby.com>
2018-04-15 21:35:03 +0100

    Removed AHK files and refactored

--- automatic/adobedigitaleditions/tools/chocolateyInstall.ps1
+++ automatic/adobedigitaleditions/tools/chocolateyInstall.ps1
@@ -1,34 +1,16 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$packageName= 'adobedigitaleditions'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://adedownload.adobe.com/pub/adobe/digitaleditions/ADE_4.5_Installer.exe'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
+    packageName   = $env:ChocolateyPackageName
+    fileType      = 'EXE'
+    url           = 'https://adedownload.adobe.com/pub/adobe/digitaleditions/ADE_4.5_Installer.exe'
+    checksum      = 'a97e536236e88f225551262fb36230a3effbbe0c83890fac28e6ce1bfcfe9ebf'
+    checksumType  = 'SHA256'
 
-  softwareName  = 'adobedigitaleditions*'
-
-  checksum      = 'a97e536236e88f225551262fb36230a3effbbe0c83890fac28e6ce1bfcfe9ebf'
-  checksumType  = 'SHA256'
-
-  silentArgs   = '/S'
-  # Note code 1223 is added as valid for v4.5.5 as it exits with this code but appears to install correctly
-  # see https://forums.adobe.com/thread/2345991
-  validExitCodes= @(0, 1223)
+    silentArgs   = '/S'
+    # Note code 1223 is added as valid for v4.5.5 as it exits with this code but appears to install correctly
+    # see https://forums.adobe.com/thread/2345991
+    validExitCodes= @(0, 1223)
 }
-
-$ahkExe = 'AutoHotKey'
-$ahkFile = Join-Path $toolsDir "adobedigitaleditions_Install.ahk"
-$ahkProc = Start-Process -FilePath $ahkExe `
-                         -ArgumentList $ahkFile `
-                         -PassThru
-
-$ahkId = $ahkProc.Id
-Write-Debug "$ahkExe start time:`t$($ahkProc.StartTime.ToShortTimeString())"
-Write-Debug "Process ID:`t$ahkId"
 
 Install-ChocolateyPackage @packageArgs
--- automatic/adobedigitaleditions/tools/chocolateyUninstall.ps1
+++ automatic/adobedigitaleditions/tools/chocolateyUninstall.ps1
@@ -1,43 +1,29 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop'
 
-$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$ahkFile = 'adobedigitaleditions_Uninstall.ahk'
-$ahkPath = $(Join-Path -Path $toolsDir -ChildPath $ahkFile)
-
-$packageName = 'adobedigitaleditions'
-$softwareName = 'adobedigitaleditions*'
-$installerType = 'EXE'
-
-$silentArgs = '/s'
-$validExitCodes = @(0)
-
-$uninstalled = $false
-[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
-
-$ahkRun = "$Env:Temp\$(Get-Random).ahk"
-write-warning "ahk file at start: $ahkrun"
-Copy-Item $ahkPath $ahkRun -Force
-$ahkProc = Start-Process -FilePath 'AutoHotKey' `
-   -ArgumentList $ahkRun `
-   -PassThru
-
-if ($key.Count -eq 1) {
-  $key | % {
-    $file = "$($_.UninstallString)"
-
-    Uninstall-ChocolateyPackage -PackageName $packageName `
-                                -FileType $installerType `
-                                -SilentArgs "$silentArgs" `
-                                -ValidExitCodes $validExitCodes `
-                                -File "$file"
-  }
-} elseif ($key.Count -eq 0) {
-  Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
-  Write-Warning "$key.Count matches found!"
-  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $_.DisplayName"}
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    softwareName   = 'Adobe Digital Editions*'
+    fileType       = 'EXE'
+    silentArgs     = '/s'
+    validExitCodes = @(0)
 }
 
-Remove-Item "$ahkRun" -Force
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs.softwareName
+if ($key.Count -eq 1) {
+    $key | ForEach-Object {
+        $packageArgs.file = $_.UninstallString
+
+        Uninstall-ChocolateyPackage @packageArgs
+    }
+}
+elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$key.Count matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | ForEach-Object {
+        Write-Warning "- $_.DisplayName"
+    }
+}
