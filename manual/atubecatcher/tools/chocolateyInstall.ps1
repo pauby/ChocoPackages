53fba51f0cf32b69fefcd693b6f042121bdf4e36
Paul Broadwith <paul@pauby.com>
2018-04-15 21:35:39 +0100

    Updated for new version

--- manual/atubecatcher/tools/chocolateyInstall.ps1
+++ manual/atubecatcher/tools/chocolateyInstall.ps1
@@ -1,18 +1,11 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-
-$packageName = $env:ChocolateyPackageName
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'http://files.dsnetwb.com/aTube_Catcher.exe'
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-    packageName    = $packageName
-    unzipLocation  = $toolsDir
-    fileType       = 'EXE' #only one of these: exe, msi, msu
-    url            = $url
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'EXE'
+    url            = 'http://files.dsnetwb.com/aTube_Catcher.exe'
 
-    softwareName   = 'atube*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-
-    checksum       = '904A4623E5A150509C06972E4AA8D61E672C3FB38DBB70BCA4C2340E5B1E4635'
+    checksum       = 'A6976364942431A09A925AA93D8EBA0AF339903D7E0253B1A4D4922363A7D6D3'
     checksumType   = 'SHA256' #default is md5, can also be sha1, sha256 or sha512
 
     silentArgs   = '/silent'
--- manual/atubecatcher/tools/chocolateyUninstall.ps1
+++ manual/atubecatcher/tools/chocolateyUninstall.ps1
@@ -1,4 +1,5 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop'
+
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = 'atube catcher*'
@@ -8,17 +9,10 @@ $packageArgs = @{
 	validExitCodes= @(0)
 }
 
-$uninstalled = $false
-
-[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
-
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs.softwareName
 if ($key.Count -eq 1) {
-    $key | % { 
-        $packageArgs['file'] = "$($_.UninstallString)"
-        if ($packageArgs['fileType'] -eq 'MSI') {
-            $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
-            $packageArgs['file'] = ''
-        }
+    $key | ForEach-Object {
+        $packageArgs.file = $_.UninstallString
 
         Uninstall-ChocolateyPackage @packageArgs
     }
@@ -27,8 +21,10 @@ elseif ($key.Count -eq 0) {
     Write-Warning "$packageName has already been uninstalled by other means."
 }
 elseif ($key.Count -gt 1) {
-    Write-Warning "$($key.Count) matches found!"
+    Write-Warning "$key.Count matches found!"
     Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
     Write-Warning "Please alert package maintainer the following keys were matched:"
-    $key | % {Write-Warning "- $($_.DisplayName)"}
+    $key | ForEach-Object {
+        Write-Warning "- $_.DisplayName"
+    }
 }
