29ed2be8d88bb5290fb4d281c8939f00fd06eb7f
Chocolatey <chocolatey@realdimensions.net>
2018-04-19 12:11:48 +0000

    AU: 1 updated - mysql-connector [skip ci]

--- automatic/mysql-connector/tools/chocolateyInstall.ps1
+++ automatic/mysql-connector/tools/chocolateyInstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
     fileType       = 'MSI'
-    url            = 'https://dev.mysql.com/get/Downloads/Connector-Net/mysql-connector-net-6.10.6.msi'
+    url            = 'https://dev.mysql.com/get/Downloads/Connector-Net/mysql-connector-net-8.0.11.msi'
 
     softwareName   = 'mysql connector net*'
 
-    checksum       = '372c900a891352f74e8e426bfa49a74948deef8a8ed829f5d4f83400d1869f8c'
+    checksum       = 'c66b1303a9a1e5a845b37cbcbf8f4dd1c63b9f1e039d60fb7c29e748bd3085f4'
     checksumType   = 'SHA256'
 
     silentArgs     = '/qn'
