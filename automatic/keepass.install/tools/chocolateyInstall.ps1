bba8043aa1645ffa69fef697e5f4adc5bc69798f
Chocolatey <chocolatey@realdimensions.net>
2018-05-06 12:09:53 +0000

    AU: 3 updated - keepass keepass.install keepass.portable [skip ci]

--- automatic/keepass.install/tools/chocolateyInstall.ps1
+++ automatic/keepass.install/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.38/KeePass-2.38-Setup.exe/download'
-    checksum       = '400b66380d30c904711ba3b017aa97f9e67081b73d6a239c6c44ac2b663cb23b'
+    url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.39/KeePass-2.39-Setup.exe/download'
+    checksum       = 'b61db93244b9594633d2e6854497c58076f93a26a0c8cbeb4d65f16ded74c578'
     checksumType   = 'sha256'
     fileType       = 'EXE'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
--- automatic/keepass.portable/tools/chocolateyInstall.ps1
+++ automatic/keepass.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.38/KeePass-2.38.zip/download'
-    checksum       = 'db71cba72c321ecaeda7eec6f4e71e215864d4113ab9e29dc97ad4d56c80a7a9'
+    url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.39/KeePass-2.39.zip/download'
+    checksum       = 'cf5efc6a4d6e3625e992765ad292bd86e73cc329c534f73d9788750bc3470c2d'
     checksumType   = 'sha256'
     UnzipLocation  = $toolsDir
 }
