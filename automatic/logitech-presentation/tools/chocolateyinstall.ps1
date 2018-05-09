2fb7a9d865d364e53dc4fd2bc77c98a871f7a02f
Paul Broadwith <pauby@users.noreply.github.com>
2018-05-09 10:55:21 +0100

    Updated missing checksum

--- automatic/logitech-presentation/tools/chocolateyinstall.ps1
+++ automatic/logitech-presentation/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     url            = 'https://download01.logi.com/web/ftp/pub/techsupport/presentation/LogiPresentation_1.40.104.exe'
-    checksum       = ''
-    checksumType   = ''
+    checksum       = '6B124E23EC0416C2BAC3A5456CE5A659121174DE5B0BE6E2B5BB9A47F837D629'
+    checksumType   = 'sha256'
     fileType       = 'EXE'
     silentArgs     = '/s'
     validExitCodes = @(0)   
