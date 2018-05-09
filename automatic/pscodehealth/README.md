96427c4fd64a5fea4817be6719955f14a04ff334
Paul Broadwith <pauby@users.noreply.github.com>
2018-05-09 21:44:40 +0100

    Initial commit

---
+++ automatic/pscodehealth/README.md
@@ -0,0 +1,17 @@
+# ![PSCodeHealth Logo](https://cdn.rawgit.com/pauby/ChocoPackages/67812184/icons/pscodehealth.png "PSCodeHealth") [PSCodeHealth](https://chocolatey.org/packages/pscodehealth)
+
+PSCodeHealth allows you to measure the quality and maintainability of your PowerShell code, based on a variety of metrics related to:
+
+* Code length
+* Code complexity
+* Code smells, styling issues and violations of best practices
+* Tests and test coverage
+* Comment-based help
+
+It can allow you to ensure that your code is compliant with metrics goals (quality gates). You can use the default (built-in) compliance rules, and you can also customize some (or all) compliance rules to fit your goals.
+
+These features can be leveraged from within your PowerShell release pipeline.
+
+PSCodeHealth can also generate a highly visual HTML report so that you can interpret the results at a glance, and easily share them.
+
+**NOTE**: This is an automatically updated package. If you find it is out of date by more than a week, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ automatic/pscodehealth/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir         = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$moduleName       = 'PSCodeHealth'  # this may be different from the package name and different case
+
+if ($PSVersionTable.PSVersion.Major -lt 5) {
+    throw "PSCodeHealth module requires a minimum of PowerShell 5."
+}
+
+# module may already be installed outside of Chocolatey
+Remove-Module -Name $moduleName -Force -ErrorAction SilentlyContinue
+
+$manifestFile = Join-Path -Path $toolsDir -ChildPath "$moduleName\$moduleName.psd1"
+$manifest     = Test-ModuleManifest -Path $manifestFile -WarningAction Ignore -ErrorAction Stop
+$sourcePath = Join-Path -Path $toolsDir -ChildPath "$modulename\*"
+$destPath = Join-Path -Path $env:ProgramFiles -ChildPath "WindowsPowerShell\Modules\$moduleName\$($manifest.Version.ToString())"
+
+Write-Verbose "Creating destination directory '$destPath' for module."
+New-Item -Path $destPath -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
+
+Write-Verbose "Moving '$moduleName' files from '$sourcePath' to '$destPath'."
+Move-Item -Path $sourcePath -Destination $destPath -Force
