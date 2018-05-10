243d144217da0bad262515c748f9bab73e870f28
Paul Broadwith <pauby@users.noreply.github.com>
2018-05-10 14:03:25 +0100

    Path typo

--- automatic/f.lux/update.ps1
+++ automatic/f.lux/update.ps1
@@ -1,7 +1,7 @@
 import-module au
 
 #Virtual package uses dependency updater to get the version
-. $PSScriptRoot\..\flux.install\update.ps1
+. $PSScriptRoot\..\f.lux.install\update.ps1
 
 function global:au_SearchReplace {
     @{
