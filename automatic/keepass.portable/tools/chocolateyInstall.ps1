52a401b5ecfd84b3cfd0c74000170cd3bf26020d
Chocolatey <chocolatey@realdimensions.net>
2018-05-14 12:11:25 +0000

    AU: 1 updated - keepass.portable [skip ci]

--- automatic/keepass.portable/tools/chocolateyInstall.ps1
+++ automatic/keepass.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.39/KeePass-2.39.zip/download'
-    checksum       = 'cf5efc6a4d6e3625e992765ad292bd86e73cc329c534f73d9788750bc3470c2d'
+    url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.39.1/KeePass-2.39.1.zip/download'
+    checksum       = '9caa97a428d8fabdaef40dec0ff9cec12b26347c6caef369f4fec7f73961469e'
     checksumType   = 'sha256'
     UnzipLocation  = $toolsDir
 }
