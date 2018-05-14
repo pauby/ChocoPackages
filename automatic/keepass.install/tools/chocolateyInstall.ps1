23ff9717e8c1d94abf20856f98c3b47930328f20
Chocolatey <chocolatey@realdimensions.net>
2018-05-14 11:45:12 +0000

    AU: 2 updated - keepass keepass.install [skip ci]

--- automatic/keepass.install/tools/chocolateyInstall.ps1
+++ automatic/keepass.install/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.39/KeePass-2.39-Setup.exe/download'
-    checksum       = 'b61db93244b9594633d2e6854497c58076f93a26a0c8cbeb4d65f16ded74c578'
+    url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.39.1/KeePass-2.39.1-Setup.exe/download'
+    checksum       = 'bbb51c65a73d5c0aa0c065f431505a2e5a52a4053528950eb8cebb0224e6b762'
     checksumType   = 'sha256'
     fileType       = 'EXE'
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
