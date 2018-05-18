01495c92467cac7659ef7c4c6ffc0bf491c28d4c
Paul Broadwith <pauby@users.noreply.github.com>
2018-05-18 15:36:45 +0100

    supporting 32 and 64 bit [build]

--- automatic/yubico-authenticator/tools/chocolateyinstall.ps1
+++ automatic/yubico-authenticator/tools/chocolateyinstall.ps1
@@ -1,13 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-    packageName   = $env:ChocolateyPackageName
-    fileType      = 'exe'
-    silentArgs    = "/S"
-    validExitCodes= @(0)
-    url           = 'https://github.com//Yubico/yubioath-desktop/releases/download/yubioath-desktop-4.3.3/yubioath-desktop-4.3.3-win.exe'
-    checksum      = '5a13fde898ab9f225cb164f737f8525e112250e95c0abed2cc87003bd3d468af'
-    checksumType  = 'sha256'
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'exe'
+    silentArgs     = "/S"
+    validExitCodes = @(0)
+    url            = 'https://github.com//Yubico/yubioath-desktop/releases/download/yubioath-desktop-4.3.4/yubioath-desktop-4.3.4-win32.exe'
+    checksum       = ''
+    checksumType   = ''
+    url64          = 'https://github.com//Yubico/yubioath-desktop/releases/download/yubioath-desktop-4.3.4/yubioath-desktop-4.3.4-win64.exe'
+    checksum64     = ''
+    checksumType64 = ''
 }
 
 Install-ChocolateyPackage @packageArgs 
