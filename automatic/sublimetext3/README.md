b348827b4da6a0254a9830a274b8bf5d9dc03f55
Paul Broadwith <pauby@users.noreply.github.com>
2018-05-18 15:31:49 +0100

    Initial commit [AU sublimetext3:3.1.1]

---
+++ automatic/sublimetext3/README.md
@@ -0,0 +1,8 @@
+# ![SublimeText3](https://cdn.rawgit.com/pauby/ChocoPackages/9bd04335/icons/sublimetext3.png "SublimeText3 Logo") [SublimeText3](https://chocolatey.org/packages/streamdeck)
+
+Sublime Text 3 is a sophisticated text editor for code, html and prose. You'll love the slick user interface and extraordinary features.
+
+_**NOTE: Sublime Text may be downloaded and evaluated for free, however a license must be purchased for continued use. Sublime Text 3 is currently in beta.**_
+.
+
+**NOTE**: This is an automatically updated package. If you find it is out of date by more than a week, please contact the maintainer(s) and let them know the package is no longer updating correctly.
---
+++ automatic/sublimetext3/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    url            = 'https://download.sublimetext.com/Sublime%20Text%20Build%203176%20Setup.exe'
+    checksum       = '7339ec15eda39c7b9328554a271cc4fc6029a3fbe9804c242be02b8c2d1854d1'
+    checksumType   = 'SHA256'
+    url64          = 'https://download.sublimetext.com/Sublime%20Text%20Build%203176%20x64%20Setup.exe'
+    checksum64     = 'a6f2652584c98b39855252feb84a830b303fd7b09002c9956f71426543403257'
+    checksumtype64 = 'SHA256'
+    fileType       = 'EXE'
+    silentArgs     = '/VERYSILENT /NORESTART /TASKS="contextentry"'
+    validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
