93c9ce5b131e2991495e7df1a76cfce9d84338db
Patrick Bußmann <patrick.bussmann@bussmann-it.de>
2018-11-21 21:47:36 +0100

    Updated automatic update for TeamViewer

--- automatic/teamviewer/update.ps1
+++ automatic/teamviewer/update.ps1
@@ -22,10 +22,11 @@ function global:au_GetLatest {
     $tempFile = New-TemporaryFile
     Invoke-WebRequest -Uri $releases -OutFile $tempFile -UseBasicParsing
     $fileVer = (Get-Item $tempfile).VersionInfo
+    $versionForURL = "$($fileVer.ProductMajorPart)"
     $version = "$($fileVer.ProductMajorPart).$($fileVer.ProductMinorPart).$($fileVer.ProductBuildPart)"
 
     return @{
-        URL32   = $releases
+        URL32   = "https://download.teamviewer.com/download/version_${versionForURL}x/TeamViewer_Setup.exe"
         Version = $version
     }
 }
