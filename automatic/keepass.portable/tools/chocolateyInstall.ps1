40905c6818c248d000450c558c2299d0d30cdbcf
Aaron Throckmorton <aaron@throckmortons.com>
2019-02-06 15:08:24 -0700

    Add unzip target parameter

--- automatic/keepass.portable/tools/chocolateyInstall.ps1
+++ automatic/keepass.portable/tools/chocolateyInstall.ps1
@@ -2,12 +2,27 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+$pp = Get-PackageParameters
+
+$config = "$toolsDir\package_install.config"
+$target = $toolsDir
+
+if ($pp["target"]) {
+    $target = $pp["target"]
+    $target | Out-File $config
+}
+elseif (Test-Path $config) {
+    $configPath = Get-Content -Path $config
+    Write-Host "configPath: $configPath"
+    if ($configPath -And (Test-Path $configPath)) { $target = configPath }
+}
+
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.39.1/KeePass-2.39.1.zip/download'
     checksum       = '9caa97a428d8fabdaef40dec0ff9cec12b26347c6caef369f4fec7f73961469e'
     checksumType   = 'sha256'
-    UnzipLocation  = $toolsDir
+    UnzipLocation  = $target
 }
 
 Install-ChocolateyZipPackage @packageArgs
