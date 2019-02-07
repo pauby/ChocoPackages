a5754219442c115dcebb2dbd96c17d0da2b81e2b
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2019-02-07 05:40:31 +0000

    Fixed incorrect URLs

--- automatic/yubikey-manager/tools/chocolateyinstall.ps1
+++ automatic/yubikey-manager/tools/chocolateyinstall.ps1
@@ -6,10 +6,10 @@ $packageArgs = @{
     silentArgs     = "/S"
     
     validExitCodes = @(0)
-    url            = 'https://github.com//Yubico/yubikey-manager-qt/releases/download/yubikey-manager-qt-0.5.1/yubikey-manager-qt-0.5.1-win32.exe'
+    url            = 'https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-1.1.0-win32.exe'
     checksum       = 'c84bc170b5e9bc4979aac242d19b80fa2b8f5587a540dcc7233ee2aafacc77f5'
     checksumType   = 'sha256'
-    url64          = 'https://github.com//Yubico/yubikey-manager-qt/releases/download/yubikey-manager-qt-0.5.1/yubikey-manager-qt-0.5.1-win64.exe'
+    url64          = 'https://developers.yubico.com/yubikey-manager-qt/Releases/yubikey-manager-qt-1.1.0-win64.exe'
     checksum64     = '1161de1953b1f5cd98be28b3117fb9972bfec08e1548cc5c08633c05b7f434ce'
     checksumType64 = 'sha256'
 }
