f33aa486da167ff21e8a4319624be0445abe1441
Paul Broadwith <paul@pauby.com>
2019-03-17 20:24:54 +0000

    Update to work with tools dir

--- automatic/keepass.portable/tools/chocolateyInstall.ps1
+++ automatic/keepass.portable/tools/chocolateyInstall.ps1
@@ -1,15 +1,14 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installDir = Join-Path -Path (Get-ToolsLocation) -ChildPath $env:ChocolateyPackageName
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     url            = 'https://sourceforge.net/projects/keepass/files/KeePass%202.x/2.39.1/KeePass-2.39.1.zip/download'
     checksum       = '9caa97a428d8fabdaef40dec0ff9cec12b26347c6caef369f4fec7f73961469e'
     checksumType   = 'sha256'
-    UnzipLocation  = $toolsDir
+    UnzipLocation  = $installDir
 }
 
 Install-ChocolateyZipPackage @packageArgs
-
-Set-Content -Path (Join-Path -Path $toolsDir -ChildPath "KeePass.exe.gui") -Value $null
+Install-BinFile -Name 'keepass' -Path (Join-Path -Path $installDir -ChildPath 'keepass.exe')
