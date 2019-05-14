df1fd9497944b42b7dd747d64ce8d523714cf501
Johannes Rehle <johannes.rehle@googlemail.com>
2019-05-15 00:20:27 +0200

    Changed the method how keepass is closed

--- automatic/keepass.install/tools/chocolateybeforemodify.ps1
+++ automatic/keepass.install/tools/chocolateybeforemodify.ps1
@@ -1,4 +1,11 @@
-﻿#Automatically close KeePass if KeePass is running.
+﻿# Automatically close KeePass if KeePass is running. If the database has unsaved changes the user will be prompted to save the database.
+# Works only with the default installation paths
 if (Get-Process -Name "Keepass" -ErrorAction "SilentlyContinue" ) { 
-    Stop-Process -Name "Keepass"
+    if (Test-Path "${env:ProgramFiles(x86)}\KeePass Password Safe 2\KeePass.exe") {
+        & "${env:ProgramFiles(x86)}\KeePass Password Safe 2\KeePass.exe" "--exit-all"
+    } else {
+        if (Test-Path "$env:ProgramFiles\KeePass Password Safe 2\KeePass.exe") {
+            & "$env:ProgramFiles\KeePass Password Safe 2\KeePass.exe" "--exit-all"
+        }
+    }
 }
