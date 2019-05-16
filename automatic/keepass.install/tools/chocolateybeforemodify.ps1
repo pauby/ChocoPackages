827db6d834c7442f4d942d51571b8507ebd676c7
Johannes Rehle <johannes.rehle@googlemail.com>
2019-05-16 21:42:04 +0200

    Inform user

--- automatic/keepass.install/tools/chocolateybeforemodify.ps1
+++ automatic/keepass.install/tools/chocolateybeforemodify.ps1
@@ -1,6 +1,7 @@
 ﻿# Automatically close KeePass if KeePass is running. If the database has unsaved changes the user will be prompted to save the database.
 # Works only with the default installation paths
 if (Get-Process -Name "Keepass" -ErrorAction "SilentlyContinue" ) { 
+    Write-Output "KeePass is running. Try to close KeePass automatically. If you have unsaved entries, you will be prompted to save them."
     if (Test-Path "${env:ProgramFiles(x86)}\KeePass Password Safe 2\KeePass.exe") {
         & "${env:ProgramFiles(x86)}\KeePass Password Safe 2\KeePass.exe" "--exit-all"
     } else {
