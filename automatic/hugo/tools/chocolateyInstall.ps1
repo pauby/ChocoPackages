061a60ede88be74371a1faa69921310ae5804432
Mikeee <hey@mike.ee>
2020-05-19 15:14:11 +0100

    update package (with -NoCheckChocoVersion)

--- automatic/hugo/tools/chocolateyInstall.ps1
+++ automatic/hugo/tools/chocolateyInstall.ps1
@@ -5,13 +5,13 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url            = 'https://github.com//gohugoio/hugo/releases/download/v0.51/hugo_0.51_Windows-32bit.zip'
-    url64          = 'https://github.com//gohugoio/hugo/releases/download/v0.51/hugo_0.51_Windows-64bit.zip'
+    url            = 'https://github.com//gohugoio/hugo/releases/download/v0.71.0/hugo_0.71.0_Windows-32bit.zip'
+    url64          = 'https://github.com//gohugoio/hugo/releases/download/v0.71.0/hugo_0.71.0_Windows-64bit.zip'
 
-    checksum       = 'BB6AE1134F2772D77A257C81DF526518406429918D636E3A0CB05D853A1E486E'
-    checksumType   = 'sha256'
-    checksum64     = 'A2ECE21BD629481E88AAFAE413FB6DA23BE3F7967753E8F09CBE40A12EC14921'
-    checksumType64 = 'sha256'
+    checksum       = '3b384bb8ace81e083d021e93f38e86207ee3eb0969d75d003eb6ab4a868301b8'
+    checksumType   = 'SHA256'
+    checksum64     = 'b84abcc4fcd5b990dab8399198eedefdf5b560aad09ece7a25c1fd806d53b2a4'
+    checksumType64 = 'SHA256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
