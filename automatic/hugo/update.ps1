c13b3505599afbf8c489e1c3198fdb53ccba49f1
Mikeee <hey@mike.ee>
2020-05-19 15:12:37 +0100

    add update checking for x86 versions of hugo

--- automatic/hugo/update.ps1
+++ automatic/hugo/update.ps1
@@ -7,6 +7,9 @@ $releases    = 'https://github.com/gohugoio/hugo/releases'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*url\s*=\s*)(''.*'')'              = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
             '(^\s*url64\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
             "(?i)(^\s*checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
@@ -15,6 +18,10 @@ function global:au_SearchReplace {
 }
 
 function global:au_BeforeUpdate {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+    $Latest.ChecksumType32 = 'SHA256'
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+    $Latest.ChecksumType64 = 'SHA256'
 }
 
 function global:au_AfterUpdate {
@@ -23,14 +30,18 @@ function global:au_AfterUpdate {
 
 function global:au_GetLatest {
     $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regexUrl = '/hugo_(?<version>[\d\.]+)_Windows-64bit\.zip'
 
-    $url = $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href
+    $regexUrl32 = '/hugo_(?<version>[\d\.]+)_Windows-32bit\.zip'
+    $regexUrl64 = '/hugo_(?<version>[\d\.]+)_Windows-64bit\.zip'
+
+    $url32 = $page.links | Where-Object href -match $regexUrl32 | Select-Object -First 1 -expand href
+    $url64 = $page.links | Where-Object href -match $regexUrl64 | Select-Object -First 1 -expand href
 
     return @{
-        URL64        = "https://github.com/$url"
+        URL32        = "https://github.com/$url32"
+        URL64        = "https://github.com/$url64"
         Version      = $matches.version
     }
 }
 
-update -ChecksumFor 64
+update -ChecksumFor none
