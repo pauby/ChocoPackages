362c34e42cfa106a336441fa41e77d2c90270996
Paul Broadwith <pauby@users.noreply.github.com>
2020-06-11 13:08:44 +0100

    Fix missing Get-MsiProductVersion function [skip ci]

--- automatic/streamdeck/update.ps1
+++ automatic/streamdeck/update.ps1
@@ -4,6 +4,49 @@
 
 $releases    = 'https://gc-updates.elgato.com/windows/sd-update/final/download-website.php'
 
+function Get-MsiProductVersion {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript( { $_ | Test-Path -PathType Leaf })]
+        [string]
+        $Path
+    )
+
+    # We need absolute paths for this to work
+    $Path = (Resolve-Path -Path $Path).Path
+
+    function Get-Property ($Object, $PropertyName, [object[]]$ArgumentList) {
+        return $Object.GetType().InvokeMember($PropertyName, 'Public, Instance, GetProperty', $null, $Object, $ArgumentList)
+    }
+
+    function Invoke-Method ($Object, $MethodName, $ArgumentList) {
+        return $Object.GetType().InvokeMember($MethodName, 'Public, Instance, InvokeMethod', $null, $Object, $ArgumentList)
+    }
+
+    $ErrorActionPreference = 'Stop'
+    Set-StrictMode -Version Latest
+
+    #http://msdn.microsoft.com/en-us/library/aa369432(v=vs.85).aspx
+    $msiOpenDatabaseModeReadOnly = 0
+    $Installer = New-Object -ComObject WindowsInstaller.Installer
+
+    $Database = Invoke-Method $Installer OpenDatabase  @($Path, $msiOpenDatabaseModeReadOnly)
+
+    $View = Invoke-Method $Database OpenView  @("SELECT Value FROM Property WHERE Property='ProductVersion'")
+
+    $null = Invoke-Method $View Execute
+
+    $Record = Invoke-Method $View Fetch
+    if ($Record) {
+        Write-Output (Get-Property $Record StringData 1)
+    }
+
+    $null = Invoke-Method $View Close @()
+    Remove-Variable -Name Record, View, Database, Installer
+}
+
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
@@ -35,4 +78,4 @@ function global:au_GetLatest {
     }
 }
 
-update -ChecksumFor none
+Update-Package -ChecksumFor none
