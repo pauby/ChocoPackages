122af24a85da7e5b724e930549bb39bb0c198ceb
Paul Broadwith <pauby@users.noreply.github.com>
2020-06-15 17:20:46 +0100

    feat: Add new package [skip ci]

---
+++ automatic/keepass-plugin-keetraytotp/tools/chocolateyInstall.ps1
@@ -0,0 +1,44 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$pluginFilename = 'KeeTrayTOTP.plgx'
+$pluginSourcePath = Join-Path -Path $toolsDir -ChildPath $pluginFilename
+
+$packageSearch = 'KeePass Password Safe*'
+$installPath = ''
+
+Write-Verbose "Searching for Keepass install location."
+if ([array]$key = Get-UninstallRegistryKey -SoftwareName $packageSearch) {
+    $installPath = $key.InstallLocation
+}
+
+if ([string]::IsNullOrEmpty($installPath)) {
+    Write-Verbose "Searching '$env:ChocolateyBinRoot' for portable install..."
+    $portPath = Join-Path -Path $env:ChocolateyBinRoot -ChildPath "keepass"
+    $installPath = Get-ChildItem -Directory "$portPath*" -ErrorAction SilentlyContinue
+}
+
+if ([string]::IsNullOrEmpty($installPath)) {
+    Write-Verbose "Searching '$env:Path' for unregistered install..."
+    $installFullName = Get-Command -Name keepass -ErrorAction SilentlyContinue
+    if ($installFullName) {
+        $installPath = Split-Path $installFullName.Path -Parent
+    }
+}
+
+if ([string]::IsNullOrEmpty($installPath)) {
+    throw "$($packageSearch) not found."
+}
+else {
+    Write-Verbose "Found Keepass install location at '$installPath'."
+}
+
+$pluginDestinationPath = Join-Path (Join-Path -Path $installPath -ChildPath 'Plugins') -ChildPath $pluginFilename
+Copy-Item -Path $pluginSourcePath -Destination $pluginDestinationPath -Force
+
+if (Get-Process -Name 'KeePass' -ErrorAction SilentlyContinue) {
+    Write-Warning "Keepass is currently running. '$($env:ChocolateyPackageName)' will be available at next restart."
+}
+else {
+    Write-Host "'$($env:ChocolateyPackageName)' will be loaded the next time KeePass is started."
+}
---
+++ automatic/keepass-plugin-keetraytotp/tools/chocolateyUninstall.ps1
@@ -0,0 +1,42 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$pluginFilename = 'KeeTrayTOTP.plgx'
+
+$packageSearch = 'KeePass Password Safe*'
+$installPath = ''
+
+Write-Verbose "Searching for Keepass install location."
+if ([array]$key = Get-UninstallRegistryKey -SoftwareName $packageSearch) {
+    $installPath = $key.InstallLocation
+}
+
+if ([string]::IsNullOrEmpty($installPath)) {
+    Write-Verbose "Searching '$env:ChocolateyBinRoot' for portable install..."
+    $portPath = Join-Path -Path $env:ChocolateyBinRoot -ChildPath "keepass"
+    $installPath = Get-ChildItem -Directory "$portPath*" -ErrorAction SilentlyContinue
+}
+
+if ([string]::IsNullOrEmpty($installPath)) {
+    Write-Verbose "Searching '$env:Path' for unregistered install..."
+    $installFullName = Get-Command -Name keepass -ErrorAction SilentlyContinue
+    if ($installFullName) {
+        $installPath = Split-Path $installFullName.Path -Parent
+    }
+}
+
+if ([string]::IsNullOrEmpty($installPath)) {
+    throw "$($packageSearch) not found."
+}
+else {
+    Write-Verbose "Found Keepass install location at '$installPath'."
+}
+
+$pluginPath = Join-Path -Path $installPath -ChildPath 'Plugins'
+Remove-Item -Path (Join-Path -Path $pluginPath -ChildPath $pluginFilename) -Force -ErrorAction SilentlyContinue
+
+if (Get-Process -Name 'KeePass' -ErrorAction SilentlyContinue) {
+    Write-Warning "Keepass is currently running. '$($env:ChocolateyPackageName)' has been removed and will be unavailable at next restart."
+}
+else {
+    Write-Host "'$($env:ChocolateyPackageName)' will be loaded the next time KeePass is started."
+}
