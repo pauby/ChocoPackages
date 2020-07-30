1f4ccba31e37e4df373818b3e317e21654879502
Paul Broadwith <paul@pauby.com>
2020-07-30 11:38:09 +0100

    fix: Embed sofwtare and fix licence url [skip_ci]

--- automatic/syncthing/update.ps1
+++ automatic/syncthing/update.ps1
@@ -12,20 +12,21 @@ function global:au_SearchReplace {
             "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
             '(^\s*url64\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
-            "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"            
+            "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
     }
 }
 
 function global:au_BeforeUpdate() {
+    Invoke-WebRequest -Uri $Latest.Url32 -UseBasicParsing -OutFile
     $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
     $Latest.ChecksumType32 = 'SHA256'
     $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
     $Latest.ChecksumType64 = 'SHA256'
 }
 
-function global:au_AfterUpdate { 
-    Set-DescriptionFromReadme -SkipFirst 2 
+function global:au_AfterUpdate {
+    Set-DescriptionFromReadme -SkipFirst 2
 }
 
 function global:au_GetLatest {
