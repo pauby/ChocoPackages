956e2d4ed6ee08f35d6568ae97b1f6ed2648d4ee
Paul Broadwith <paul@pauby.com>
2022-03-01 20:50:42 +0000

    (pkg) Initial commit

---
+++ automatic/vivaldi.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+    packageName    = 'vivaldi'
+    fileType       = 'exe'
+
+    url            = 'https://downloads.vivaldi.com/stable-auto/Vivaldi.5.1.2567.49.exe'
+    url64bit       = 'https://downloads.vivaldi.com/stable-auto/Vivaldi.5.1.2567.49.x64.exe'
+
+    checksum       = '928C93FB2FFD2D33B3CFD062054451404AB763180C3CF158949F1FDB4FA9D995'
+    checksumType   = 'sha256'
+
+    checksum64     = 'C1867D9181133E9F5403E58C84FD680B2FC52C8FC87240908371CC4A1D73B90D'
+    checksumType64 = 'sha256'
+
+    silentArgs     = '--vivaldi-silent --do-not-launch-chrome'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/vivaldi.portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    softwareName   = 'vivaldi*'
+    fileType       = 'EXE'
+    silentArgs     = '--uninstall --force-uninstall --vivaldi'
+    validExitCodes = @(0, 19)   # no matter the combination of switches I use, 19 is always the exit code
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs.softwareName
+if ($key.Count -eq 1) {
+    $key | ForEach-Object {
+        # The uninstall string contains the `--uninstall` switch - we just want the file path
+        $packageArgs.file = $_.UninstallString.replace(' --uninstall', '')
+
+        Uninstall-ChocolateyPackage @packageArgs
+    }
+}
+elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+    Write-Warning "$key.Count matches found!"
+    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+    Write-Warning "Please alert package maintainer the following keys were matched:"
+    $key | ForEach-Object {
+        Write-Warning "- $_.DisplayName"
+    }
+}
