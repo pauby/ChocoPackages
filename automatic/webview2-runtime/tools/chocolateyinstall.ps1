9b5c1415f3a7a8103dae02a327ea00dc3cff72b5
Paul Broadwith <paul@pauby.com>
2022-03-12 23:00:12 +0000

    (pkg) Initial commit

---
+++ automatic/webview2-runtime/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  url           = 'https://go.microsoft.com/fwlink/p/?LinkId=2124703'
+
+  softwareName  = 'Microsoft Edge WebView2*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  checksum      = '502b8140500de1b863bd018130de17afd95ca2d0bffc0d4dbf4964fd9f78cabf'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/silent /install'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/webview2-runtime/tools/chocolateyuninstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Microsoft Edge WebView2 Runtime'
+  fileType      = 'EXE'
+  # MSI
+  silentArgs    = '--uninstall --msedgewebview --system-level --verbose-logging --force-uninstall'
+  validExitCodes = @(0, 19)   # no matter the combination of switches I use, 19 is always the exit code
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+    $packageArgs['file'] = ($key.UninstallString).replace(' --uninstall --msedgewebview --system-level --verbose-logging', '')
+
+    Uninstall-ChocolateyPackage @packageArgs
+}
+elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+}
+elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
