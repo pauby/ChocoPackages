550fb831cc0d1fc093785d974f97685286bc7907
Paul Broadwith <paul@pauby.com>
2022-04-26 18:10:32 +0100

    (#137) (bind-toolsonly) Added updated package

---
+++ manual/bind-toolsonly/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path -Path $MyInvocation.MyCommand.Definition
+$extractDir = Join-Path -Path $toolsDir -ChildPath 'content'
+
+$packageArgs = @{
+    packageName     = 'bind'
+    fileFullPath64  = Join-Path -Path $toolsDir -ChildPath 'BIND9.16.28.x64.zip'
+    destination     = $extractDir
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item -Path (Join-Path -Path $toolsDir -ChildPath 'BIND9.16.28.x64.zip') -Force
