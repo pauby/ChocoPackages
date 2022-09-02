1e304729d63c5a2ad45e16fcee7f37b7cc987e5b
Klaus <dieechtenilente@users.noreply.github.com>
2022-09-02 19:24:04 +0200

    Updated setup checksums

--- automatic/teamviewer/tools/chocolateyInstall.ps1
+++ automatic/teamviewer/tools/chocolateyInstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   softwareName  = 'TeamViewer*'
 
   url           = 'https://download.teamviewer.com/download/TeamViewer_Setup.exe'
-  checksum      = 'f60062cf21ed42ba0adf64a296f124074ef4ad92b6b58e2f488c4b028a286bf4'
+  checksum      = 'f2aa91feccea466bd9c0440db1ade6e16b10f42897e30431996fc4d02ed500f5'
   checksumType  = 'sha256'
 
   url64         = 'https://download.teamviewer.com/download/TeamViewer_Setup_x64.exe'
-  checksum64    = 'efa1b635356ef73d7a61486ba89401b46cc7cf5a71f7d179beafe59152b2f8f6'
+  checksum64    = 'fccef4dd9b654f98c100ab41a8e7e01d6257d496827caa740097e8a82e3e2054'
   checksumType64= 'sha256'
 
   silentArgs    = "/S"
