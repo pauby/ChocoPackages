590ef362c15b818a88b1f77f09864829385e2905
Paul Broadwith <paul@pauby.com>
2022-09-21 20:51:49 +0100

    (hugo) Embedded package and removed redundant uninstall script

--- automatic/hugo/tools/chocolateyInstall.ps1
+++ automatic/hugo/tools/chocolateyInstall.ps1
@@ -1,14 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$zipFile = 'hugo_0.103.1_windows-amd64.zip'
+$filePath = Join-Path -Path $toolsDir -ChildPath $zipFile
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
-    unzipLocation  = $toolsDir
-    url64          = 'https://github.com//gohugoio/hugo/releases/download/v0.71.0/hugo_0.71.0_Windows-64bit.zip'
-
-    checksum64     = 'b84abcc4fcd5b990dab8399198eedefdf5b560aad09ece7a25c1fd806d53b2a4'
-    checksumType64 = 'SHA256'
+    fileFullPath64 = $filePath
+    destination    = $toolsDir
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
