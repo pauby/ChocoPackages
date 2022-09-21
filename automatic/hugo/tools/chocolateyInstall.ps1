f884fa0269861cb4c9b24ba05953cf620160da06
Paul Broadwith <paul@pauby.com>
2022-09-21 17:10:07 +0100

    (hugo) Removing x86 and updating for new filenames
    x86 support was removed at some point. Unsure when.

--- automatic/hugo/tools/chocolateyInstall.ps1
+++ automatic/hugo/tools/chocolateyInstall.ps1
@@ -5,11 +5,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
-    url            = 'https://github.com//gohugoio/hugo/releases/download/v0.71.0/hugo_0.71.0_Windows-32bit.zip'
     url64          = 'https://github.com//gohugoio/hugo/releases/download/v0.71.0/hugo_0.71.0_Windows-64bit.zip'
 
-    checksum       = '3b384bb8ace81e083d021e93f38e86207ee3eb0969d75d003eb6ab4a868301b8'
-    checksumType   = 'SHA256'
     checksum64     = 'b84abcc4fcd5b990dab8399198eedefdf5b560aad09ece7a25c1fd806d53b2a4'
     checksumType64 = 'SHA256'
 }
--- automatic/hugo/update.ps1
+++ automatic/hugo/update.ps1
@@ -7,9 +7,6 @@ $releases    = 'https://github.com/gohugoio/hugo/releases'
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            '(^\s*url\s*=\s*)(''.*'')'              = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
             '(^\s*url64\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum64)'"
             "(?i)(^\s*checksumType64\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType64)'"
@@ -18,8 +15,6 @@ function global:au_SearchReplace {
 }
 
 function global:au_BeforeUpdate {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
-    $Latest.ChecksumType32 = 'SHA256'
     $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
     $Latest.ChecksumType64 = 'SHA256'
 }
@@ -31,14 +26,11 @@ function global:au_AfterUpdate {
 function global:au_GetLatest {
     $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $regexUrl32 = '/hugo_(?<version>[\d\.]+)_Windows-32bit\.zip'
-    $regexUrl64 = '/hugo_(?<version>[\d\.]+)_Windows-64bit\.zip'
+    $regexUrl64 = '/hugo_(?<version>[\d\.]+)_windows-amd64\.zip'
 
-    $url32 = $page.links | Where-Object href -match $regexUrl32 | Select-Object -First 1 -expand href
     $url64 = $page.links | Where-Object href -match $regexUrl64 | Select-Object -First 1 -expand href
 
     return @{
-        URL32        = "https://github.com/$url32"
         URL64        = "https://github.com/$url64"
         Version      = $matches.version
     }
