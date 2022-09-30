4a23f7de71349c835410db7b1ab840f87b515fff
Paul Broadwith <paul@pauby.com>
2022-09-30 17:34:19 +0100

    (gh) Update to use GitHub API

--- automatic/gh/tools/chocolateyInstall.ps1
+++ automatic/gh/tools/chocolateyInstall.ps1
@@ -2,12 +2,13 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $logMsi = Join-Path -Path $env:TEMP -ChildPath ("{0}-{1}-MsiInstall.log" -f $env:ChocolateyPackageName, $env:chocolateyPackageVersion)
+$installerFile = 'gh_2.16.1_windows_amd64.msi'
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'MSI'
     silentArgs     = "/qn /norestart /l*v `"$logMsi`""
-    file64         = Join-Path -Path $toolsDir -ChildPath "gh_$($env:ChocolateyPackageVersion)_windows_amd64.msi"
+    file64         = Join-Path -Path $toolsDir -ChildPath $installerFile
 }
 
 Install-ChocolateyInstallPackage @packageArgs
--- automatic/gh/update.ps1
+++ automatic/gh/update.ps1
@@ -2,22 +2,20 @@
 
 . $PSScriptRoot\..\..\scripts\all.ps1
 
-$releases    = 'https://github.com/cli/cli/releases' # no trailing slash!
+Import-Module PowerShellForGitHub
+$repoOwner = 'cli'
+$repoName = 'cli'
 
 function global:au_SearchReplace {
     @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            '(^\s*\$installerFile\s*=\s*)(''.*'')' = "`$1'$($Latest.Filename)'"
+        }
     }
 }
 
 function global:au_BeforeUpdate {
-    do {
-        $tempPath = Join-Path -Path $env:TEMP -ChildPath ([GUID]::NewGuid()).ToString()
-    }
-    while (Test-Path $tempPath)
-
-    New-Item -Path $tempPath -ItemType Directory | Out-Null
-    $filename = "gh_{0}_windows_amd64.msi" -f $Latest.Version
-    Invoke-WebRequest -Uri $Latest.URL64 -UseBasicParsing -OutFile (Join-Path -Path 'tools' -ChildPath $filename)
+    Get-GitHubReleaseAsset -OwnerName $repoOwner -RepositoryName $repoName -Asset $Latest.ReleaseAssetID -Path "tools\$($Latest.Filename)" -Force
 }
 
 function global:au_AfterUpdate {
@@ -25,13 +23,21 @@ function global:au_AfterUpdate {
 }
 
 function global:au_GetLatest {
-    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regexUrl = '/download/v(?<version>[\d\.]+)/gh_[\d\.]+_windows_amd64.msi'
-    $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href
+
+    $release = Get-GitHubRelease -OwnerName $repoOwner -RepositoryName $repoName -Latest
+    $version = $release.tag_name
+    if ($version.StartsWith('v')) {
+        $version = $version.Substring(1)    # skip over 'v' in tag
+    }
+
+    $asset = Get-GitHubReleaseAsset -OwnerName $repoOwner -RepositoryName $repoName -Release $release.id | Where-Object name -Match "gh_$($version)_windows_amd64.msi"
+    $url = $asset.browser_download_url
 
     return @{
-        URL64 = ("{0}{1}" -f $releases, $matches[0])
-        Version = $matches.version
+        ReleaseAssetID = $asset.id
+        Filename       = $asset.name
+        URL32          = $url
+        Version        = $version
     }
 }
 
