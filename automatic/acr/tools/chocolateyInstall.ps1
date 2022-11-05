a13091b433d65f4eb18a6430dad31508c99ebe18
Paul Broadwith <paul@pauby.com>
2022-11-05 19:31:50 +0000

    (acr) Change to embedded package and Use GitHub API

--- automatic/acr/tools/chocolateyInstall.ps1
+++ automatic/acr/tools/chocolateyInstall.ps1
@@ -1,24 +1,16 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'acr'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com//acreloaded/acr/releases/download/v2.6.3/acr_02_06_03-w.zip'
 $executable = "client.bat"
+$zip32Filename = 'acr_v2.18.2-w.zip'
 
 $packageArgs = @{
-    packageName    = $packageName
-    unzipLocation  = $toolsDir
-    url            = $url
-
-    softwareName   = 'acr*'
-
-    checksum       = '522163f665802d2b38c58cad3bddb98f4a236b6e8927bc7c179c82a19cd63db2'
-    checksumType   = 'SHA256'
-
-    validExitCodes = @(0)
+    packageName    = $env:ChocolateyPackageName
+    fileFullPath   = Join-Path -Path $toolsdir -ChildPath $zip32Filename
+    Destination    = $toolsDir
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnZip @packageArgs
 Install-ChocolateyShortcut -ShortcutFilePath (Join-Path -Path $env:ALLUSERSPROFILE -ChildPath "Microsoft\Windows\Start Menu\Programs\AC Reloaded.lnk") `
 	-TargetPath (Join-Path -Path $packageArgs.unzipLocation -ChildPath $executable) `
 	-WorkingDirectory $toolsdir
