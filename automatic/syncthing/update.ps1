2ebccda84283bf9824f830e9659226273856820f
Paul Broadwith <paul@pauby.com>
2022-11-05 18:45:17 +0000

    (syncthing) Update to use GitHub API

--- automatic/syncthing/update.ps1
+++ automatic/syncthing/update.ps1
@@ -2,25 +2,25 @@
 
 . $PSScriptRoot\..\..\scripts\all.ps1
 
-Import-Module PowerShellForGitHub
-Set-GitHubConfiguration -DisableTelemetry
-
 $repoOwner = 'syncthing'
 $repoName = 'syncthing'
 
 function global:au_SearchReplace {
     @{
+        ".\syncthing.nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
         ".\tools\chocolateyInstall.ps1" = @{
-            '(^\s*\$zip32Filename\s*=\s*)(''.*'')' = "`$1'$($Latest.Filename32)'"
-            '(^\s*\$zip64Filename\s*=\s*)(''.*'')' = "`$1'$($Latest.Filename64)'"
+            '(^\s*\$zip32Filename\s*=\s*)(''.*'')' = "`$1'$($Latest.Asset32.name)'"
+            '(^\s*\$zip64Filename\s*=\s*)(''.*'')' = "`$1'$($Latest.Asset64.name)'"
         }
     }
 }
 
 function global:au_BeforeUpdate() {
     Get-RemoteFiles -Purge
-    Get-GitHubReleaseAsset -OwnerName $repoOwner -RepositoryName $repoName -Asset $Latest.ReleaseAsset32ID -Path "tools\$($Latest.Filename32)" -Force
-    Get-GitHubReleaseAsset -OwnerName $repoOwner -RepositoryName $repoName -Asset $Latest.ReleaseAsset64ID -Path "tools\$($Latest.Filename64)" -Force
+    Invoke-WebRequest -Uri $Latest.URL32 -OutFile "tools\$($Latest.Asset32.name)"
+    Invoke-WebRequest -Uri $Latest.URL64 -OutFile "tools\$($Latest.Asset64.name)"
 }
 
 function global:au_AfterUpdate {
@@ -29,42 +29,23 @@ function global:au_AfterUpdate {
 
 function global:au_GetLatest {
 
-    $release = Get-GitHubRelease -OwnerName $repoOwner -RepositoryName $repoName -Latest
+    $release = Get-GitHubRelease -OwnerName $repoOwner -RepositoryName $repoName -Tag 'Latest'
     $version = $release.tag_name
     if ($version.StartsWith('v')) {
         $version = $version.Substring(1)    # skip over 'v' in tag
     }
 
-    $asset = Get-GitHubReleaseAsset -OwnerName $repoOwner -RepositoryName $repoName -Release $release.id
-    $asset32 = $asset | Where-Object name -match 'syncthing-windows-386-v(?<version>.*).zip'
-    $asset64 = $asset | Where-Object name -Match 'syncthing-windows-amd64-v(?<version>.*).zip'
+    $asset32 = $release.assets | Where-Object name -match 'syncthing-windows-386-v(?<version>.*).zip'
+    $asset64 = $release.assets | Where-Object name -Match 'syncthing-windows-amd64-v(?<version>.*).zip'
 
     return @{
-        ReleaseAsset32ID = $asset32.id
-        ReleaseAsset64ID = $asset64.id
-        Filename32       = $asset32.name
-        Filename64       = $asset64.name
+        Asset32          = $asset32
+        Asset64          = $asset64
         URL32            = $asset32.browser_download_url
         URL64            = $asset64.browser_download_url
         Version          = $version
+        ReleaseNotes    = $release.body
     }
-
-
-
-
-    # $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    # $regexUrl = 'syncthing-windows-386-v(?<version>.*).zip'
-
-    # $url32 = $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href
-    # $version = $matches.version
-    # $url64 = $url32 -replace '-386-', '-amd64-'
-
-    # return @{
-    #     URL32   = "https://github.com/$url32"
-    #     URL64   = "https://github.com/$url64"
-    #     Version = $version
-    #     PayloadVersion = $version
-    # }
 }
 
 Update-Package -ChecksumFor none
