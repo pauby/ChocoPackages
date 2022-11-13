51a6be6c3aa95729086df3b59611ab8fbf654c8f
Paul Broadwith <paul@pauby.com>
2022-11-13 18:47:04 +0000

    (acr) Add release notes

--- automatic/acr/update.ps1
+++ automatic/acr/update.ps1
@@ -1,40 +1,57 @@
-#import-module au
-
 . $PSScriptRoot\..\..\scripts\all.ps1
 
-$releases    = 'https://github.com/acreloaded/acr/releases/latest'
+$repoOwner = 'acreloaded'
+$repoName = 'acr'
 
 function global:au_SearchReplace {
     @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$([System.Web.HttpUtility]::HtmlEncode($Latest.ReleaseNotes))`$2"
+        }
         ".\tools\chocolateyInstall.ps1" = @{
-            '(^\s*\$url\s*=\s*)(''.*'')'            = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"       = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksumType\s*=\s*)('.*')"   = "`$1'$($Latest.ChecksumType32)'"
+            '(^\s*\$zip32Filename\s*=\s*)(''.*'')' = "`$1'$($Latest.Asset32.name)'"
+        }
+        ".\tools\VERIFICATION.txt"      = @{
+            "(^\s*x86 URL:\s*)(.*)"           = "`$1$($Latest.URL32)"
+            "(^\s*x86 Checksum Type:\s*)(.*)" = "`$1$($Latest.ChecksumType32)"
+            "(^\s*x86 Checksum:\s*)(.*)"      = "`${1}$($Latest.Checksum32)"
         }
     }
 }
 
 function global:au_BeforeUpdate() {
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+    Remove-Item -Path 'tools\*.zip' -Force -ErrorAction SilentlyContinue
+    Invoke-WebRequest -Uri $Latest.URL32 -OutFile "tools\$($Latest.Asset32.name)"
     $Latest.ChecksumType32 = 'SHA256'
+    $Latest.Checksum32 = (Get-FileHash -Path "tools\$($Latest.Asset32.name)" -Algorithm $Latest.ChecksumType32).Hash
 }
 
-function global:au_AfterUpdate { 
-    Set-DescriptionFromReadme -SkipFirst 2 
+function global:au_AfterUpdate {
+    Set-DescriptionFromReadme -SkipFirst 2
 }
 
-function global:au_GetLatest {
-    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regexUrl = "acr_(.*)-w.zip"
+function global:au_GetLatest
+{
+    $release = Get-GitHubRelease -OwnerName $repoOwner -RepositoryName $repoName -Tag 'Latest'
+    $version = $release.tag_name
+    if ($version.StartsWith('v')) {
+        $version = $version.Substring(1)    # skip over 'v' in tag
+    }
+    $releaseNotes = if ([string]::IsNullOrEmpty($release.body)) {
+        $release.html_url
+    }
+    else {
+        $release.body
+    }
 
-    $url = $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href
-    $url -match "/download/v(.*)/" | Out-Null
-    $version = $matches[1]
+    $asset32 = $release.assets | Where-Object name -Match 'acr_(.*)-w.zip'
 
     return @{
-        URL32        = "https://github.com/$url"
+        Asset32      = $asset32
+        URL32        = $asset32.browser_download_url
         Version      = $version
+        ReleaseNotes = $releaseNotes
     }
 }
 
-update -ChecksumFor none
+Update-Package -ChecksumFor none
