b2c72258c4c8a53e15cc15913206380f67137df2
Paul Broadwith <paul@pauby.com>
2023-05-28 16:01:12 +0100

    (webview2-runtime) Updated API endpoint and body

--- automatic/webview2-runtime/update.ps1
+++ automatic/webview2-runtime/update.ps1
@@ -2,7 +2,7 @@
 
 . $PSScriptRoot\..\..\scripts\all.ps1
 
-$releases = 'https://msedge.api.cdp.microsoft.com/api/v1.1/contents/Browser/namespaces/Default/names/msedgewebview-stable-win-x64/versions/latest?action=select'
+$releases = 'https://msedge.api.cdp.microsoft.com/api/v2/contents/Browser/namespaces/Default/names?action=batchupdates'
 
 function global:au_SearchReplace {
     @{
@@ -21,44 +21,9 @@ function global:au_AfterUpdate {
 }
 
 function global:au_GetLatest {
-    $body = @{
-        targetingAttributes = @{
-            AppAp = ""
-            AppBrandCode = ""
-            AppCohort = ""
-            AppCohortHint = ""
-            AppCohortName = ""
-            AppLang = ""
-            AppMajorVersion = ""
-            AppRollout = 0.06
-            AppTargetVersionPrefix = ""
-            AppVersion = ""
-            ExpETag = ""
-            HW_AVX = $true
-            HW_DiskType = 2
-            HW_LogicalCpus = 12
-            HW_PhysicalRamGB = 64
-            HW_SSE = $true
-            HW_SSE2 = $true
-            HW_SSE3 = $true
-            HW_SSE41 = $true
-            HW_SSE42 = $true
-            HW_SSSE3 = $true
-            InstallSource = "taggedmi"
-            IsInternalUser = $false
-            IsMachine = $false
-            OemProductManufacturer = "System manufacturer"
-            OemProductName = "System Product Name"
-            OsArch = "x64"
-            OsPlatform = "win"
-            OsVersion = "10.0.19041.1415"
-            Priority = 10
-            Updater = "MicrosoftEdgeUpdate"
-            UpdaterVersion = "1.3.155.85"
-        }
-    }
-
-    $response = Invoke-RestMethod -Uri $releases -Method Post -Body $body -UseBasicParsing
+    $jsonString = '[ { "Product":"msedgewebview-stable-win-x64", "targetingAttributes": { "AppAp":"", "AppBrandCode":"", "AppCohort":"", "AppCohortHint":"", "AppCohortName":"", "AppLang":"", "AppMajorVersion":"", "AppRollout":0.29, "AppTargetVersionPrefix":"", "AppVersion":"", "ExpETag":"", "HW_AVX":true, "HW_DiskType":2, "HW_LogicalCpus":2, "HW_PhysicalRamGB":4, "HW_SSE":true, "HW_SSE2":true, "HW_SSE3":true, "HW_SSE41":true, "HW_SSE42":true, "HW_SSSE3":true, "InstallSource":"taggedmi", "IsInternalUser":false, "IsMachine":true, "IsWIP":false, "OemProductManufacturer":"System Manufacturer", "OemProductName":"System Product Name", "OsArch":"x64", "OsPlatform":"win", "OsVersion":"10.0.17763.3887", "Priority":10, "Updater":"MicrosoftEdgeUpdate", "UpdaterVersion":"1.3.175.27","WIPBranch":""}}]'
+    
+    $response = Invoke-RestMethod -Uri $releases -Method Post -Body $jsonString -UseBasicParsing -ContentType 'application/json' -UserAgent 'Microsoft Edge Update/1.3.175.27;winhttp'
 
     $version = $response.ContentId.Version
     $url32 = 'https://go.microsoft.com/fwlink/p/?LinkId=2124703'
