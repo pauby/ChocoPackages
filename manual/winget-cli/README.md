80b5278c09966c92d01e2d18988d29953c117234
Paul Broadwith <paul@pauby.com>
2023-06-03 16:52:25 +0100

    (winget-cli) Initial commit

---
+++ manual/winget-cli/README.md
@@ -0,0 +1,5 @@
+﻿# ![WinGet-CLI](https://cdn.jsdelivr.net/gh/pauby/ChocoPackages@cd7c485/icons/winget-cli.png "WinGet-CLI Logo") [WinGet-CLI](https://chocolatey.org/packages/winget-cli)
+
+The winget command line tool enables users to discover, install, upgrade, remove and configure applications on Windows 10 and Windows 11 computers. This tool is the client interface to the Windows Package Manager service.
+
+**NOTE**: This is a manually updated package.
---
+++ manual/winget-cli/tools/chocolateyuninstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$internalAppXPackage = @{
+    SoftwareName = 'WinGet-CLI'
+    PackageName  = 'Microsoft.DesktopAppInstaller'
+    Version      = '2023.417.2324.0'
+}
+
+$installedAppXPackage = Get-AppxProvisionedPackage -Online | Where-Object -Property DisplayName -EQ $internalAppXPackage.PackageName
+
+Write-Host "Removing version $($installedAppXPackage.Version) of $($internalAppXPackage.PackageName)."
+$null = Remove-AppxProvisionedPackage -PackageName $installedAppXPackage.PackageName -Online
