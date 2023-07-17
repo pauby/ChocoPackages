4c6f90c841a4530cc6d1338ff87820d9fead45a0
Paul Broadwith <paul@pauby.com>
2023-07-17 20:53:01 +0100

    (adobedigitaleditions) Use curl.exe

--- automatic/adobedigitaleditions/update.ps1
+++ automatic/adobedigitaleditions/update.ps1
@@ -24,16 +24,29 @@ function global:au_AfterUpdate {
 }
 
 function global:au_GetLatest {
-    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    # This needs to use curl.exe as it supports HTTP/2 and the Invoke-WebRequest cmdlet doesn't
+    # This website needs HTTP/2
+    $page = curl.exe $releases
 
-    $regexVer  = "Adobe Digital Editions (.*) Installers"
+    $regexVer = "Adobe Digital Editions (?<version>[\d\.]+) Installers"
     $regexUrl = "ADE_.*_installer.exe"
-    if ($page.content -match $regexVer) { $version = $matches[1] }
-    $url = $page.links | Where-Object href -match $regexUrl | Select-Object -First 1 -expand href
+
+    # There must be a better way of doing this
+    # This code is garbage.
+    # When doing a -match against the $page it returns the actual line that matches, as an array, and 
+    # not a boolean (and $matches with the results).
+    # So we take the first element of the array, do another regex match and get the values we need
+    if (($page -match $regexVer)[0] -match $regexVer) {
+        $version = $matches['version']
+    }
+    
+    ($page -match $regexUrl)[0] -match $regexUrl | Out-Null
+    $url = "https://adedownload.adobe.com/pub/adobe/digitaleditions/$($matches[0])"
 
     return @{
         URL32        = $url
         Version      = $version
+
     }
 }
 
