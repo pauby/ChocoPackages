e1ee4ed72c66b9c4d3371560e6ee51a5d24b6b8d
Paul Broadwith <paul@pauby.com>
2024-02-24 20:55:04 +0000

    fix: (emacs) use version of latest approved emacs.portable [skip ci]

--- automatic/emacs/update.ps1
+++ automatic/emacs/update.ps1
@@ -2,8 +2,6 @@
 
 . $PSScriptRoot\..\..\scripts\all.ps1
 
-$releases = 'https://ftp.gnu.org/gnu/emacs/windows' # no trailing slash
-
 function global:au_SearchReplace {
     @{
         ".\emacs.nuspec" = @{
@@ -20,43 +18,11 @@ function global:au_AfterUpdate {
 }
 
 function global:au_GetLatest {
-    $page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    # get all major versions that are available (28, 29, 30 etc.), choose the latest and open that page
-    $majorVer = @()
-    ForEach ($p in $page.links.href) {
-        if ($p -match 'emacs-(?<version>[\d]+)') {
-            $majorVer += $matches.version
-        }
-    }
+    $version = (choco search emacs.portable --exact --by-id-only --limit-output | ConvertFrom-Csv -Delimiter '|' -Header 'name', 'version').version
 
-    # we assume at this stage that they are sorted in the correct order so we just grab the last one
-    $majorVer = $majorVer | Select-Object -Unique -Last 1
-
-    # get all of the files within the page
-    $downloadPage = Invoke-WebRequest "$releases/emacs-$majorVer" -UseBasicParsing
-
-    $versions = @()
-    ForEach ($v in $downloadPage.links.href) {
-        if ($v -match 'emacs-(?<version>[\d\.]+)-') {
-            $versions += $matches.version
-        }
-    }
-    # we assume at this stage that they are sorted in the correct order so we just grab the last one 
-    $version = $versions | Select-Object -Unique -Last 1
-    $softwareVersion = $version     # this stores the emacs version
-
-    # we should use a 3 part version number
-    if (([version]$version).minor -eq -1) {
-        $version += '.0.0'
-    }
-    elseif (([version]$version).build -eq -1) {
-        $version += '.0'
-    }
     return @{
-        URL64           = "$releases/emacs-$majorVer/emacs-$softwareVersion.zip"
-        SoftwareVersion = $softwareVersion
-        Version         = $version
+        Version = ConvertTo-VersionNumber -Version ([version]$version) -Part 3
     }
 }
 
