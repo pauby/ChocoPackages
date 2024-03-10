cdef2bbd639123e6a962aed3d0b6da2a28993e78
Paul Broadwith <paul@pauby.com>
2024-03-10 15:24:59 +0000

    refactor(keepass-plugin-favicon)(#191): added new package

---
+++ manual/keepass-plugin-favicon/tools/chocolateyInstall.ps1
@@ -0,0 +1,67 @@
+# powershell v2 compatibility
+$psVer = $PSVersionTable.PSVersion.Major
+if ($psver -ge 3) {
+  function Get-ChildItemDir {Get-ChildItem -Directory $args}
+} else {
+  function Get-ChildItemDir {Get-ChildItem $args}
+}
+
+$packageName = 'keepass-plugin-favicon'
+$packageSearch = 'KeePass Password Safe'
+$url = 'http://sourceforge.net/projects/keepass-favicon/files/1.9.0/KeePassFaviconDownloader.plgx/download'
+$checksum = '595fe8aa10f72b6670be00f04aff4733d6de61d2'
+$checksumType = 'sha1'
+
+try {
+Write-Verbose "Searching registry for installed KeePass..."
+$regPath = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                            -ErrorAction:SilentlyContinue `
+           | Where-Object {$_.DisplayName -like "$packageSearch*" `
+                           -and `
+                           $_.DisplayVersion -ge 2.09 `
+                           -and `
+                           $_.DisplayVersion -lt 3.0 } `
+           | ForEach-Object {$_.InstallLocation}
+$installPath = $regPath
+if (! $installPath) {
+  Write-Verbose "Searching $env:ChocolateyBinRoot for portable install..."
+  $binRoot = Get-BinRoot
+  $portPath = Join-Path $binRoot "keepass"
+  $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
+}
+if (! $installPath) {
+  Write-Verbose "Searching $env:Path for unregistered install..."
+  $installFullName = (Get-Command keepass -ErrorAction SilentlyContinue).Path
+  if (! $installFullName) {
+    $installPath = [io.path]::GetDirectoryName($installFullName)
+  }
+}
+if (! $installPath) {
+  Write-Warning "$($packageSearch) not found."
+  throw
+}
+Write-Verbose "`t...found."
+
+Write-Verbose "Searching for plugin directory..."
+$pluginPath = (Get-ChildItemDir $installPath\Plugin*).FullName
+if ($pluginPath.Count -eq 0) {
+  $pluginPath = Join-Path $installPath "Plugins"
+  [System.IO.Directory]::CreateDirectory($pluginPath)
+}
+$installFile = Join-Path $pluginPath "$($packageName).plgx"
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                      -FileFullPath "$installFile" `
+                      -Url "$url" `
+                      -Checksum "$checksum" `
+                      -ChecksumType "$checksumType"
+if ( Get-Process -Name "KeePass" `
+                 -ErrorAction SilentlyContinue ) {
+  Write-Warning "$($packageSearch) is currently running. Plugin will be available at next restart of $($packageSearch)." 
+} else {
+  Write-Host "$($packageName) will be loaded the next time KeePass is started."
+  Write-Host "Please note this plugin may require additional configuration. Look for a new entry in KeePass' Menu>Tools"
+}} catch {
+  throw $_.Exception
+}
