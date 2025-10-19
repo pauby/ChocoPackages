faaa9d555abed05f8b25e95cbbb4ec40af3820fa
Paul Broadwith <paul@pauby.com>
2025-10-19 20:07:09 +0100

    refactor(pia): throw when server os detected

--- automatic/pia/tools/chocolateyInstall.ps1
+++ automatic/pia/tools/chocolateyInstall.ps1
@@ -2,6 +2,13 @@
 
 $toolsDir = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
 
+# Not supported on Server OS's but can be installed.
+if ($env:OS_NAME -like "*Server*") {
+    Write-Warning "The software is only supported on Windows 10 and Windows 11, and not on Windows Server operating systems."
+    Write-Warning "See the Windows downlaod page at https://www.privateinternetaccess.com/download/windows-vpn"
+    throw
+}
+
 $packageArgs = @{
     packageName     = $env:ChocolateyPackageName
     fileType        = 'exe'
